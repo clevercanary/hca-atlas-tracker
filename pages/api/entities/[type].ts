488c0a6d86051b3a0c5d51cfa9b966958ada3842
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2024-03-12 17:33:10 -0700

    feat: get user identity from authorization token (#32) (#42)

--- pages/api/entities/[type].ts
+++ pages/api/entities/[type].ts
@@ -1,9 +1,13 @@
 import fsp from "fs/promises";
+import { OAuth2Client, TokenInfo } from "google-auth-library";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const atlasesPath = "files/out/atlases.json";
 const componentAtlasesPath = "files/out/component-atlases.json";
 const sourceDatasetsPath = "files/out/source-datasets.json";
+
+const authClient = new OAuth2Client();
+const accessTokensInfo = new Map<string, TokenInfo>();
 
 let atlasesData: Buffer;
 let componentAtlasesData: Buffer;
@@ -17,11 +21,28 @@ export default async function handler(
 ): Promise<void> {
   await entitiesLoaded;
   const type = req.query.type as string;
+  const accessTokenInfo = await getAccessTokenInfo(req.headers.authorization);
+  console.log(accessTokenInfo?.email, accessTokenInfo?.email_verified);
   if (type === "atlases") sendJsonData(res, atlasesData);
   else if (type === "component-atlases")
     sendJsonData(res, componentAtlasesData);
   else if (type === "source-datasets") sendJsonData(res, sourceDatasetsData);
   else res.status(404).end();
+}
+
+async function getAccessTokenInfo(
+  authorization: string | undefined
+): Promise<TokenInfo | null> {
+  if (!authorization) return null;
+  const token = /^Bearer (.+)$/.exec(authorization)?.[1];
+  if (!token) return null;
+  let tokenInfo = accessTokensInfo.get(token);
+  if (!tokenInfo)
+    accessTokensInfo.set(
+      token,
+      (tokenInfo = await authClient.getTokenInfo(token))
+    );
+  return tokenInfo;
 }
 
 function sendJsonData(res: NextApiResponse, data: Buffer): void {
