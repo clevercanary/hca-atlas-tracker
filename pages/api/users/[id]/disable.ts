78371921d8b7094b8bbbfef5cb003370dc86dd9b
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-03-26 18:28:33 -0700

    feat: return error 404 from enable/disable when user doesn't exist (#40)

--- pages/api/users/[id]/disable.ts
+++ pages/api/users/[id]/disable.ts
@@ -12,8 +12,14 @@ export default handler(
   async (req, res) => {
     const id = handleRequiredParam(req, res, "id", /^\d+$/);
     if (id === null) return;
-    // TODO what happens when the user doesn't exist?
-    await query("UPDATE hat.users SET disabled=true WHERE id=$1", [id]);
+    const queryResult = await query(
+      "UPDATE hat.users SET disabled=true WHERE id=$1",
+      [id]
+    );
+    if (queryResult.rowCount === 0) {
+      res.status(404).end();
+      return;
+    }
     res.status(200).end();
   }
 );
