455a41ec826e1acd81c91ceb2a7f393dcef26008
Dave Rogers <dave@clevercanary.com>
2024-04-04 00:37:33 -0700

    feat: db setup scripts (#56)

---
+++ db_scripts/create-users.sql
@@ -0,0 +1,26 @@
+-- Create the 'hat' schema
+CREATE SCHEMA IF NOT EXISTS hat AUTHORIZATION postgres;
+
+-- Step 1: Create Users
+CREATE ROLE hat_migrate WITH LOGIN PASSWORD 'yourpassword';
+CREATE ROLE hat_app WITH LOGIN PASSWORD 'yourpassword';
+
+-- Step 2: Grant Permissions to Use the 'hat' Schema
+GRANT USAGE ON SCHEMA hat TO hat_migrate;
+GRANT USAGE ON SCHEMA hat TO hat_app;
+GRANT CREATE ON SCHEMA hat TO hat_migrate;
+
+-- Step 3: Grant Permissions on Existing and Future Tables
+-- Grant hat_migrate permission to manage all current and future tables in the 'hat' schema
+GRANT ALL ON ALL TABLES IN SCHEMA hat TO hat_migrate;
+ALTER DEFAULT PRIVILEGES FOR ROLE hat_migrate IN SCHEMA hat
+GRANT ALL ON TABLES TO hat_migrate;
+
+-- Grant hat_app permission to read, insert, update, and delete data in all current and future tables in the 'hat' schema
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA hat TO hat_app;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA hat
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO hat_app;
+
+-- Step 4: Grant Connect to the Database
+GRANT CONNECT ON DATABASE hcaatlastracker TO hat_migrate;
+GRANT CONNECT ON DATABASE hcaatlastracker TO hat_app;
