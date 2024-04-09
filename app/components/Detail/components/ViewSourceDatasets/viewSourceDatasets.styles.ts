b82807ce490ffc3fe0ca48b690d2ba3f76e63c51
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-04-09 11:42:38 +1000

    feat: updated source datasets table ui (#69)

--- app/components/Detail/components/ViewSourceDatasets/viewSourceDatasets.styles.ts
+++ app/components/Detail/components/ViewSourceDatasets/viewSourceDatasets.styles.ts
@@ -1,5 +1,6 @@
 import { FluidPaper } from "@clevercanary/data-explorer-ui/lib/components/common/Paper/paper.styles";
 import { TableToolbar as DXTableToolbar } from "@clevercanary/data-explorer-ui/lib/components/Table/components/TableToolbar/tableToolbar.styles";
+import { smokeLightest } from "@clevercanary/data-explorer-ui/lib/styles/common/mixins/colors";
 import styled from "@emotion/styled";
 
 export const Paper = styled(FluidPaper)`
@@ -7,6 +8,12 @@ export const Paper = styled(FluidPaper)`
     display: grid;
     gap: inherit;
     grid-column: 1 / -1;
+  }
+
+  .MuiTable-root {
+    th {
+      background-color: ${smokeLightest};
+    }
   }
 `;
 
