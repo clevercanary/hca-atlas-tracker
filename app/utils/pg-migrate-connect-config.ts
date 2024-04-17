2a1c84cb47a09544d30d32a47bd1b55219c5f188
David Rogers <dave@clevercanary.com>
2024-04-17 14:15:54 -0700

    fix: update user to username (#116) (#118)

--- app/utils/pg-migrate-connect-config.ts
+++ app/utils/pg-migrate-connect-config.ts
@@ -2,7 +2,8 @@ import pg from "pg";
 
 export function getPoolConfig(): pg.PoolConfig {
   if (process.env.APP_ENV === "aws-dev" || process.env.APP_ENV === "aws-prod") {
-    const user = JSON.parse(process.env.PG_MIGRATE_USER || "{}")["user"] || "";
+    const user =
+      JSON.parse(process.env.PG_MIGRATE_USER || "{}")["username"] || "";
     const password =
       JSON.parse(process.env.PG_MIGRATE_USER || "{}")["password"] || "";
 
