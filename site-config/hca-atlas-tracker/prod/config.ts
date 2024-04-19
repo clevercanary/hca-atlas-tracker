aa29a757454bce1ccde0bea46971fcd4a47787d8
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-04-19 15:33:14 +1000

    feat: add prod authentication config (#126)

--- site-config/hca-atlas-tracker/prod/config.ts
+++ site-config/hca-atlas-tracker/prod/config.ts
@@ -1,10 +1,14 @@
 import { SiteConfig } from "../../common/entities";
 import { makeConfig } from "../local/config";
+import { authenticationConfig } from "./authentication/authentication";
 
 // Template constants
 const BROWSER_URL = "https://tracker.data.humancellatlas.org";
 const PORTAL_URL = "https://data.humancellatlas.org";
 
 const config: SiteConfig = makeConfig(BROWSER_URL, PORTAL_URL);
+
+// Add authentication to the config.
+config.authentication = authenticationConfig;
 
 export default config;
