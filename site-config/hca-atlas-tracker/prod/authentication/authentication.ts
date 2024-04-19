67ffe4800c2cd86ad759864a62d8b7aec85c835b
Fran McDade <frano-m@users.noreply.github.com>
2024-04-19 15:36:38 +1000

    feat: add prod authentication config (#126) (#127)

---
+++ site-config/hca-atlas-tracker/prod/authentication/authentication.ts
@@ -0,0 +1,15 @@
+import { AuthenticationConfig } from "@databiosphere/findable-ui/lib/config/entities";
+import * as MDX from "../../../../app/components/common/MDXContent";
+
+export const authenticationConfig: AuthenticationConfig = {
+  googleGISAuthConfig: {
+    clientId:
+      "602867726547-8fggua16ofh4inipdvgnil9brmt3c75b.apps.googleusercontent.com",
+    googleProfileEndpoint: "https://www.googleapis.com/oauth2/v3/userinfo",
+    scope:
+      "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid",
+  },
+  termsOfService: MDX.LoginTermsOfService({}),
+  text: MDX.LoginText({}),
+  title: "Sign in to your account",
+};
