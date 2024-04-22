c7b008fb17ac42b50f59100a4d75982bfd81f161
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-04-21 20:45:05 -0700

    docs: add comment to makeRefreshService (#99)

--- app/services/common/refresh-service.ts
+++ app/services/common/refresh-service.ts
@@ -26,6 +26,17 @@ export class RefreshDataNotReadyError extends Error {
   name = "RefreshDataNotReadyError";
 }
 
+/**
+ * Initialize data that will be refreshed using a given set of functions under a given condition.
+ * @param param0 - Object containing parameters that determine the behavior of the refresh service.
+ * @param param0.getRefreshedData - Function taking refresh params and optional previous data value, which refreshes the data and returns it directly or as a promise.
+ * @param param0.getRefreshParams - Function taking optional current data value, which gets any values needed to do a refresh and returns them directly or as a promise.
+ * @param param0.getStoredInfo - Function that returns the cached info object containing the data (or undefined if it doesn't exist), e.g. from a global variable.
+ * @param param0.notReadyMessage - Message to use for an error when an attempt to access the data is made before it's initially retrieved.
+ * @param param0.refreshNeeded - Function taking optional current data value and required refresh params, which returns a boolean indicating whether a refresh is needed.
+ * @param param0.setStoredInfo - Function that takes the info object containing the data and stores it as a cache, e.g. in a global variable.
+ * @returns object containing `getData` function that returns the current data value and starts a refresh if needed.
+ */
 export function makeRefreshService<TData, TRefreshParams>({
   getRefreshedData,
   getRefreshParams,
