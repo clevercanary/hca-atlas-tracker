db004a8214ee33932a6c2fdb6d25e4176f4243f8
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-05-03 13:20:51 +1000

    feat: updated dialog content (#133)

--- app/hooks/useFormManager/common/utils.ts
+++ app/hooks/useFormManager/common/utils.ts
@@ -6,6 +6,7 @@ import { RouteValue } from "../../../routes/entities";
  * @param path - Path.
  * @param _route - Route (unused).
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- _route is unused.
 export function navigateToRoute(path: string, _route?: RouteValue): void {
   Router.push(path);
 }
