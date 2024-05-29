65b817d94d6cb6f4f9b54de6a43a5e49501cdb95
Fran McDade <frano-m@users.noreply.github.com>
2024-05-29 18:11:49 +1000

    feat: add confidentiality notice (#211) (#238)
    * feat: add confidentiality notice (#211)
    
    * Update app/components/common/MDXContent/confidentialNotice.mdx
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- app/components/common/MDXContent/index.tsx
+++ app/components/common/MDXContent/index.tsx
@@ -1,3 +1,4 @@
 export { RenderComponent } from "@databiosphere/findable-ui/lib/components/ComponentCreator/components/RenderComponent/renderComponent";
+export { default as ConfidentialNotice } from "./confidentialNotice.mdx";
 export { default as LoginTermsOfService } from "./loginTermsOfService.mdx";
 export { default as LoginText } from "./loginText.mdx";
---
+++ site-config/hca-atlas-tracker/local/viewList/subTitleHero.ts
@@ -0,0 +1,27 @@
+import {
+  ComponentConfig,
+  ComponentsConfig,
+} from "@databiosphere/findable-ui/lib/config/entities";
+import { HCAAtlasTrackerListAtlas } from "../../../../app/apis/catalog/hca-atlas-tracker/common/entities";
+import * as C from "../../../../app/components";
+import * as MDX from "../../../../app/components/common/MDXContent/index";
+
+export const subTitleHero: ComponentsConfig = [
+  {
+    children: [
+      {
+        children: [
+          {
+            component: MDX.ConfidentialNotice,
+          } as ComponentConfig<typeof MDX.ConfidentialNotice>,
+        ],
+        component: C.AlertText,
+      } as ComponentConfig<typeof C.AlertText>,
+    ],
+    component: C.FluidAlert,
+    props: {
+      severity: "warning",
+      title: "This tracker is confidential.",
+    },
+  } as ComponentConfig<typeof C.FluidAlert, HCAAtlasTrackerListAtlas>,
+];
