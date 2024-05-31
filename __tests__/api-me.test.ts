175dd82820871f8f22fce5f9ee5cf3470f5dba90
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-05-30 23:14:59 -0700

    feat: update last login when /api/me is called (#247)

--- __tests__/api-me.test.ts
+++ __tests__/api-me.test.ts
@@ -2,12 +2,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 import httpMocks from "node-mocks-http";
 import {
   HCAAtlasTrackerActiveUser,
+  HCAAtlasTrackerDBUser,
   ROLE,
 } from "../app/apis/catalog/hca-atlas-tracker/common/entities";
 import { METHOD } from "../app/common/entities";
-import { endPgPool } from "../app/services/database";
+import { endPgPool, query } from "../app/services/database";
 import meHandler from "../pages/api/me";
-import { USER_STAKEHOLDER, USER_UNREGISTERED } from "../testing/constants";
+import {
+  USER_CONTENT_ADMIN,
+  USER_STAKEHOLDER,
+  USER_UNREGISTERED,
+} from "../testing/constants";
 import { resetDatabase } from "../testing/db-utils";
 import { TestUser } from "../testing/entities";
 
@@ -15,8 +20,12 @@ jest.mock("../app/services/user-profile");
 jest.mock("../app/services/hca-projects");
 jest.mock("../app/utils/pg-app-connect-config");
 
+let initialUsers: HCAAtlasTrackerDBUser[];
+
 beforeAll(async () => {
   await resetDatabase();
+  initialUsers = (await query<HCAAtlasTrackerDBUser>("SELECT * FROM hat.users"))
+    .rows;
 });
 
 afterAll(() => {
@@ -28,10 +37,12 @@ describe("/api/me", () => {
     expect(
       (await doMeRequest(USER_STAKEHOLDER, METHOD.POST))._getStatusCode()
     ).toEqual(405);
+    await expectUsersToBeUnchanged();
   });
 
   it("returns error 401 for logged out user", async () => {
     expect((await doMeRequest())._getStatusCode()).toEqual(401);
+    await expectUsersToBeUnchanged();
   });
 
   it("returns information for logged in unregistered user", async () => {
@@ -41,17 +52,41 @@ describe("/api/me", () => {
     expect(user.email).toEqual(USER_UNREGISTERED.email);
     expect(user.fullName).toEqual(USER_UNREGISTERED.name);
     expect(user.role).toEqual(ROLE.UNREGISTERED);
+    await expectUsersToBeUnchanged();
   });
 
   it("returns information for logged in user", async () => {
+    const requestTime = Date.now();
     const res = await doMeRequest(USER_STAKEHOLDER);
     expect(res.statusCode).toEqual(200);
     const user: HCAAtlasTrackerActiveUser = res._getJSONData();
     expect(user.email).toEqual(USER_STAKEHOLDER.email);
     expect(user.fullName).toEqual(USER_STAKEHOLDER.name);
     expect(user.role).toEqual(USER_STAKEHOLDER.role);
+    const userStakeholderFromDb = (
+      await query<HCAAtlasTrackerDBUser>(
+        "SELECT * FROM hat.users WHERE email=$1",
+        [USER_STAKEHOLDER.email]
+      )
+    ).rows[0];
+    const lastLoginTime = userStakeholderFromDb.last_login.getTime();
+    expect(Math.abs(lastLoginTime - requestTime)).toBeLessThan(1000);
+    const userContentAdminFromDb = (
+      await query<HCAAtlasTrackerDBUser>(
+        "SELECT * FROM hat.users WHERE email=$1",
+        [USER_CONTENT_ADMIN.email]
+      )
+    ).rows[0];
+    expect(userContentAdminFromDb.last_login.getFullYear()).toEqual(1970);
   });
 });
+
+async function expectUsersToBeUnchanged(): Promise<void> {
+  const currentUsers = (
+    await query<HCAAtlasTrackerDBUser>("SELECT * FROM hat.users")
+  ).rows;
+  expect(currentUsers).toEqual(initialUsers);
+}
 
 async function doMeRequest(
   user?: TestUser,
