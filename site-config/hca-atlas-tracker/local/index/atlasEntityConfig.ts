efe47a2d1264d1b1f6016e5b2af064a5b54810dc
Fran McDade <frano-m@users.noreply.github.com>
2024-06-04 18:19:08 +1000

    feat: fix target completion sorting (#269) (#270)

--- site-config/hca-atlas-tracker/local/index/atlasEntityConfig.ts
+++ site-config/hca-atlas-tracker/local/index/atlasEntityConfig.ts
@@ -11,6 +11,8 @@ import {
   getAtlasId,
 } from "../../../../app/apis/catalog/hca-atlas-tracker/common/utils";
 import * as C from "../../../../app/components";
+import { mapSelectCategoryValue } from "../../../../app/config/utils";
+import { formatDateToQuarterYear } from "../../../../app/utils/date-fns";
 import * as V from "../../../../app/viewModelBuilders/catalog/hca-atlas-tracker/common/viewModelBuilders";
 import {
   HCA_ATLAS_TRACKER_CATEGORY_KEY,
@@ -46,6 +48,9 @@ export const atlasEntityConfig: EntityConfig = {
           {
             key: HCA_ATLAS_TRACKER_CATEGORY_KEY.TARGET_COMPLETION_DATE,
             label: HCA_ATLAS_TRACKER_CATEGORY_LABEL.TARGET_COMPLETION_DATE,
+            mapSelectCategoryValue: mapSelectCategoryValue(
+              formatDateToQuarterYear
+            ),
           },
           {
             key: HCA_ATLAS_TRACKER_CATEGORY_KEY.WAVE,
--- site-config/hca-atlas-tracker/local/index/tasksEntityConfig.ts
+++ site-config/hca-atlas-tracker/local/index/tasksEntityConfig.ts
@@ -16,6 +16,8 @@ import {
   taskInputMapper,
 } from "../../../../app/apis/catalog/hca-atlas-tracker/common/utils";
 import * as C from "../../../../app/components";
+import { mapSelectCategoryValue } from "../../../../app/config/utils";
+import { formatDateToQuarterYear } from "../../../../app/utils/date-fns";
 import * as V from "../../../../app/viewModelBuilders/catalog/hca-atlas-tracker/common/viewModelBuilders";
 import {
   HCA_ATLAS_TRACKER_CATEGORY_KEY,
@@ -42,6 +44,9 @@ export const tasksEntityConfig: EntityConfig = {
           {
             key: HCA_ATLAS_TRACKER_CATEGORY_KEY.TARGET_COMPLETION_DATE,
             label: HCA_ATLAS_TRACKER_CATEGORY_LABEL.TARGET_COMPLETION_DATE,
+            mapSelectCategoryValue: mapSelectCategoryValue(
+              formatDateToQuarterYear
+            ),
           },
           {
             key: HCA_ATLAS_TRACKER_CATEGORY_KEY.DESCRIPTION,
