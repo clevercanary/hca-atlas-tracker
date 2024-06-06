f245454a91a0f95d4aeaf09a1c9fc852c2c1e373
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-06-06 14:48:29 -0700

    feat: add component atlases apis (#271)

---
+++ pages/api/atlases/[atlasId]/component-atlases.ts
@@ -0,0 +1,23 @@
+import { ROLE_GROUP } from "../../../../app/apis/catalog/hca-atlas-tracker/common/constants";
+import { dbComponentAtlasToApiComponentAtlas } from "../../../../app/apis/catalog/hca-atlas-tracker/common/utils";
+import { METHOD } from "../../../../app/common/entities";
+import { getAtlasComponentAtlases } from "../../../../app/services/component-atlases";
+import { handler, method, role } from "../../../../app/utils/api-handler";
+
+/**
+ * API route for getting an atlas's component atlases.
+ */
+export default handler(
+  method(METHOD.GET),
+  role(ROLE_GROUP.READ),
+  async (req, res) => {
+    const atlasId = req.query.atlasId as string;
+    res
+      .status(200)
+      .json(
+        (await getAtlasComponentAtlases(atlasId)).map(
+          dbComponentAtlasToApiComponentAtlas
+        )
+      );
+  }
+);
