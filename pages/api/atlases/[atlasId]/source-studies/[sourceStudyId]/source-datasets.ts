479d3eb5ba12b36fc68754896c9994bc61ebf21c
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-06-07 17:52:30 -0700

    feat: add source datasets apis (#274)

---
+++ pages/api/atlases/[atlasId]/source-studies/[sourceStudyId]/source-datasets.ts
@@ -0,0 +1,24 @@
+import { ROLE_GROUP } from "../../../../../../app/apis/catalog/hca-atlas-tracker/common/constants";
+import { dbSourceDatasetToApiSourceDataset } from "../../../../../../app/apis/catalog/hca-atlas-tracker/common/utils";
+import { METHOD } from "../../../../../../app/common/entities";
+import { getSourceStudyDatasets } from "../../../../../../app/services/source-datasets";
+import { handler, method, role } from "../../../../../../app/utils/api-handler";
+
+/**
+ * API route for getting a source study's source datasets.
+ */
+export default handler(
+  method(METHOD.GET),
+  role(ROLE_GROUP.READ),
+  async (req, res) => {
+    const atlasId = req.query.atlasId as string;
+    const sourceStudyId = req.query.sourceStudyId as string;
+    res
+      .status(200)
+      .json(
+        (await getSourceStudyDatasets(atlasId, sourceStudyId)).map(
+          dbSourceDatasetToApiSourceDataset
+        )
+      );
+  }
+);
---
+++ pages/api/atlases/[atlasId]/source-studies/[sourceStudyId]/source-datasets/[sourceDatasetId].ts
@@ -0,0 +1,64 @@
+import { ROLE } from "app/apis/catalog/hca-atlas-tracker/common/entities";
+import { sourceDatasetEditSchema } from "app/apis/catalog/hca-atlas-tracker/common/schema";
+import { ROLE_GROUP } from "../../../../../../../app/apis/catalog/hca-atlas-tracker/common/constants";
+import { dbSourceDatasetToApiSourceDataset } from "../../../../../../../app/apis/catalog/hca-atlas-tracker/common/utils";
+import { METHOD } from "../../../../../../../app/common/entities";
+import {
+  deleteSourceDataset,
+  getSourceDataset,
+  updateSourceDataset,
+} from "../../../../../../../app/services/source-datasets";
+import {
+  handleByMethod,
+  handler,
+  role,
+} from "../../../../../../../app/utils/api-handler";
+
+const getHandler = handler(role(ROLE_GROUP.READ), async (req, res) => {
+  const atlasId = req.query.atlasId as string;
+  const sourceStudyId = req.query.sourceStudyId as string;
+  const sourceDatasetId = req.query.sourceDatasetId as string;
+  res
+    .status(200)
+    .json(
+      dbSourceDatasetToApiSourceDataset(
+        await getSourceDataset(atlasId, sourceStudyId, sourceDatasetId)
+      )
+    );
+});
+
+const patchHandler = handler(role(ROLE.CONTENT_ADMIN), async (req, res) => {
+  const atlasId = req.query.atlasId as string;
+  const sourceStudyId = req.query.sourceStudyId as string;
+  const sourceDatasetId = req.query.sourceDatasetId as string;
+  const inputData = await sourceDatasetEditSchema.validate(req.body);
+  res
+    .status(200)
+    .json(
+      dbSourceDatasetToApiSourceDataset(
+        await updateSourceDataset(
+          atlasId,
+          sourceStudyId,
+          sourceDatasetId,
+          inputData
+        )
+      )
+    );
+});
+
+const deleteHandler = handler(role(ROLE.CONTENT_ADMIN), async (req, res) => {
+  const atlasId = req.query.atlasId as string;
+  const sourceStudyId = req.query.sourceStudyId as string;
+  const sourceDatasetId = req.query.sourceDatasetId as string;
+  await deleteSourceDataset(atlasId, sourceStudyId, sourceDatasetId);
+  res.status(200).end();
+});
+
+/**
+ * API route for getting, updating, or deleting a source dataset.
+ */
+export default handleByMethod({
+  [METHOD.GET]: getHandler,
+  [METHOD.PATCH]: patchHandler,
+  [METHOD.DELETE]: deleteHandler,
+});
