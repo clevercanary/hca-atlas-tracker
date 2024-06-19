eb53856ba801d0d0ec27101ce2cd772bac31ddfc
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-06-19 16:37:42 +1000

    feat: make atlas pages (add and view atlas) use a generic atlas form (#318)

---
+++ app/components/common/Form/components/FormManager/components/AccessPrompt/accessPrompt.tsx
@@ -0,0 +1,29 @@
+import { Link } from "@databiosphere/findable-ui/lib/components/Links/components/Link/link";
+import { ReactNode } from "react";
+import { ROUTE } from "../../../../../../../routes/constants";
+import {
+  Section,
+  SectionHero,
+  SectionText,
+} from "../../../../../../Detail/components/TrackerForm/components/Section/section.styles";
+
+interface AccessPromptProps {
+  divider?: ReactNode;
+  text: ReactNode;
+}
+
+export const AccessPrompt = ({
+  divider = null,
+  text,
+}: AccessPromptProps): JSX.Element => {
+  return (
+    <Section>
+      {divider}
+      <SectionHero>
+        <SectionText>
+          <Link label="Sign in" url={ROUTE.LOGIN} /> {text}.
+        </SectionText>
+      </SectionHero>
+    </Section>
+  );
+};
