108350e55874b0d4ff3ef5617ead20aa8a1710cb
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2024-06-23 00:47:32 -0500

    feat: add suspension type column to source datasets tables (#339) (#342)

--- app/viewModelBuilders/catalog/hca-atlas-tracker/common/constants.ts
+++ app/viewModelBuilders/catalog/hca-atlas-tracker/common/constants.ts
@@ -9,5 +9,6 @@ export const PLURALIZED_METADATA_LABEL = {
   [M.DISEASE]: "diseases",
   [M.LIBRARY_CONSTRUCTION_METHOD]: "library construction methods",
   [M.SPECIES]: "species",
+  [M.SUSPENSION_TYPE]: "suspension types",
   [M.TISSUE]: "tissues",
 };
