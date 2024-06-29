f68a6c9e6bb1d6c11491dbfb9b89fdf07004ec4a
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-06-28 21:39:49 -0700

    feat: add stub of migration for validation comments (#347)

---
+++ migrations/1719633686503_validation-comment-thread-property.ts
@@ -0,0 +1,12 @@
+import { MigrationBuilder } from "node-pg-migrate";
+
+export function up(pgm: MigrationBuilder): void {
+  pgm.addColumns(
+    { name: "validations", schema: "hat" },
+    {
+      comment_thread_id: {
+        type: "uuid",
+      },
+    }
+  );
+}
