6e77164fad5d0262c69d1eed94d0e9208ff2ddcb
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-07-08 21:47:49 -0700

    feat: allow integration leads to use source dataset apis for their atlases (#370)

--- pages/api/atlases/[atlasId]/source-studies/[sourceStudyId]/source-datasets/[sourceDatasetId].ts
+++ pages/api/atlases/[atlasId]/source-studies/[sourceStudyId]/source-datasets/[sourceDatasetId].ts
@@ -11,6 +11,7 @@ import {
 import {
   handleByMethod,
   handler,
+  integrationLeadAssociatedAtlasOnly,
   role,
 } from "../../../../../../../app/utils/api-handler";
 
@@ -27,32 +28,40 @@ const getHandler = handler(role(ROLE_GROUP.READ), async (req, res) => {
     );
 });
 
-const patchHandler = handler(role(ROLE.CONTENT_ADMIN), async (req, res) => {
-  const atlasId = req.query.atlasId as string;
-  const sourceStudyId = req.query.sourceStudyId as string;
-  const sourceDatasetId = req.query.sourceDatasetId as string;
-  const inputData = await sourceDatasetEditSchema.validate(req.body);
-  res
-    .status(200)
-    .json(
-      dbSourceDatasetToApiSourceDataset(
-        await updateSourceDataset(
-          atlasId,
-          sourceStudyId,
-          sourceDatasetId,
-          inputData
+const patchHandler = handler(
+  role([ROLE.INTEGRATION_LEAD, ROLE.CONTENT_ADMIN]),
+  integrationLeadAssociatedAtlasOnly,
+  async (req, res) => {
+    const atlasId = req.query.atlasId as string;
+    const sourceStudyId = req.query.sourceStudyId as string;
+    const sourceDatasetId = req.query.sourceDatasetId as string;
+    const inputData = await sourceDatasetEditSchema.validate(req.body);
+    res
+      .status(200)
+      .json(
+        dbSourceDatasetToApiSourceDataset(
+          await updateSourceDataset(
+            atlasId,
+            sourceStudyId,
+            sourceDatasetId,
+            inputData
+          )
         )
-      )
-    );
-});
+      );
+  }
+);
 
-const deleteHandler = handler(role(ROLE.CONTENT_ADMIN), async (req, res) => {
-  const atlasId = req.query.atlasId as string;
-  const sourceStudyId = req.query.sourceStudyId as string;
-  const sourceDatasetId = req.query.sourceDatasetId as string;
-  await deleteSourceDataset(atlasId, sourceStudyId, sourceDatasetId);
-  res.status(200).end();
-});
+const deleteHandler = handler(
+  role([ROLE.INTEGRATION_LEAD, ROLE.CONTENT_ADMIN]),
+  integrationLeadAssociatedAtlasOnly,
+  async (req, res) => {
+    const atlasId = req.query.atlasId as string;
+    const sourceStudyId = req.query.sourceStudyId as string;
+    const sourceDatasetId = req.query.sourceDatasetId as string;
+    await deleteSourceDataset(atlasId, sourceStudyId, sourceDatasetId);
+    res.status(200).end();
+  }
+);
 
 /**
  * API route for getting, updating, or deleting a source dataset.
