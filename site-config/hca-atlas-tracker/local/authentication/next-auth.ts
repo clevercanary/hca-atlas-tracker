196d6a40812efeeefd4c61d8b3ea4272fbfdd931
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-07-31 15:10:24 -0700

    feat: add minimal azure ad login button (#395)

--- site-config/hca-atlas-tracker/local/authentication/next-auth.ts
+++ site-config/hca-atlas-tracker/local/authentication/next-auth.ts
@@ -1,3 +1,4 @@
+import AzureAdProvider from "next-auth/providers/azure-ad";
 import GoogleProvider from "next-auth/providers/google";
 
 export const nextAuthOptions = {
@@ -9,6 +10,11 @@ export const nextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? "",
       clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+    AzureAdProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID ?? "",
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET ?? "",
+      tenantId: process.env.AZURE_AD_TENANT_ID,
     }),
   ],
 };
