d842ca83528ff826bc88a8b664c3ee0d5e11d2a7
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-08-01 13:21:42 -0700

    feat: allow stakeholder-like roles to get users (#400)

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,8 +1,6 @@
-import { dbUserToApiUser } from "app/apis/catalog/hca-atlas-tracker/common/utils";
-import {
-  HCAAtlasTrackerDBUserWithAssociatedResources,
-  ROLE,
-} from "../../app/apis/catalog/hca-atlas-tracker/common/entities";
+import { ROLE_GROUP } from "../../app/apis/catalog/hca-atlas-tracker/common/constants";
+import { HCAAtlasTrackerDBUserWithAssociatedResources } from "../../app/apis/catalog/hca-atlas-tracker/common/entities";
+import { dbUserToApiUser } from "../../app/apis/catalog/hca-atlas-tracker/common/utils";
 import { METHOD } from "../../app/common/entities";
 import { getAllUsers, getUserByEmail } from "../../app/services/users";
 import {
@@ -17,7 +15,7 @@ import {
  */
 export default handler(
   method(METHOD.GET),
-  role(ROLE.CONTENT_ADMIN),
+  role(ROLE_GROUP.READ),
   async (req, res) => {
     let users: HCAAtlasTrackerDBUserWithAssociatedResources[];
     if (typeof req.query.email === "string") {
