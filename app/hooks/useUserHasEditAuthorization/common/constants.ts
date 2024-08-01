7a2fc83aceb5ab07ddbce79d9cc07a8e9ca33860
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-08-01 23:00:48 +1000

    feat: integration leads can manage integration objects for their atlases - fe (#394)

--- app/hooks/useUserHasEditAuthorization/common/constants.ts
+++ app/hooks/useUserHasEditAuthorization/common/constants.ts
@@ -2,6 +2,9 @@ import { ROUTE } from "../../../routes/constants";
 import { RouteValue } from "../../../routes/entities";
 
 export const ROUTES: RouteValue[] = [
+  ROUTE.COMPONENT_ATLAS,
+  ROUTE.COMPONENT_ATLASES,
+  ROUTE.CREATE_COMPONENT_ATLAS,
   ROUTE.CREATE_SOURCE_STUDY,
   ROUTE.SOURCE_DATASETS,
   ROUTE.SOURCE_STUDIES,
