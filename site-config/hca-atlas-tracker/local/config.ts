c65ab1dc8ae8a357f4fc9fd1ba156caa0c368086
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-08-15 12:38:32 -0700

    fix: use nextauth-based authentication in header (#395)

--- site-config/hca-atlas-tracker/local/config.ts
+++ site-config/hca-atlas-tracker/local/config.ts
@@ -8,6 +8,7 @@ import {
   TEXT_HEADING_XLARGE,
 } from "@databiosphere/findable-ui/lib/theme/common/typography";
 import * as C from "../../../app/components/index";
+import { Authentication } from "../../../app/components/Layout/components/Header/components/Content/components/Actions/components/Authentication/authentication";
 import { ROUTE } from "../../../app/routes/constants";
 import { SiteConfig } from "../../common/entities";
 import { announcementsConfig } from "./announcements/announcementsConfig";
@@ -55,6 +56,7 @@ export function makeConfig(
       },
       header: {
         announcements: announcementsConfig,
+        authenticationComponent: Authentication,
         authenticationEnabled: true,
         logo: C.Logo({
           alt: APP_TITLE,
