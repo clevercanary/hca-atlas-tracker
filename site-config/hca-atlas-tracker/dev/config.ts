04d5089cbd11ecf26fdca91fd9a5b1b510860867
Fran McDade <frano-m@users.noreply.github.com>
2024-11-21 10:50:13 +1000

    feat: add version info to footer (#459) (#460)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- site-config/hca-atlas-tracker/dev/config.ts
+++ site-config/hca-atlas-tracker/dev/config.ts
@@ -1,10 +1,14 @@
 import { SiteConfig } from "../../common/entities";
-import { makeConfig, PORTAL_URL } from "../local/config";
+import { GIT_HUB_REPO_URL, makeConfig, PORTAL_URL } from "../local/config";
 
 // Template constants
 const BROWSER_URL =
   "https://test-tracker.data.humancellatlas.dev.clevercanary.com";
 
-const config: SiteConfig = makeConfig(BROWSER_URL, PORTAL_URL);
+const config: SiteConfig = makeConfig(
+  BROWSER_URL,
+  PORTAL_URL,
+  GIT_HUB_REPO_URL
+);
 
 export default config;
