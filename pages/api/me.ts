ee7e1ab46f55209f2c7344d9e810ce9b21e931dd
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-11-25 00:32:23 -0800

    feat: create account automatically via `me` api (#464)

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -4,37 +4,37 @@ import {
 } from "../../app/apis/catalog/hca-atlas-tracker/common/entities";
 import { METHOD } from "../../app/common/entities";
 import { getProvidedUserProfile } from "../../app/services/user-profile";
-import { updateLastLogin } from "../../app/services/users";
+import { createUser, updateLastLogin } from "../../app/services/users";
 import {
   getUserFromAuthorization,
   handler,
   method,
+  UnauthenticatedError,
 } from "../../app/utils/api-handler";
 
 export default handler(method(METHOD.GET), async (req, res) => {
   const userProfile = await getProvidedUserProfile(req.headers.authorization);
-  const user = await getUserFromAuthorization(req.headers.authorization);
-  let activeUserInfo: HCAAtlasTrackerActiveUser;
-  if (user) {
-    await updateLastLogin(user.id);
-    activeUserInfo = {
-      disabled: user.disabled,
-      email: user.email,
-      fullName: user.full_name,
-      role: user.role,
-      roleAssociatedResourceIds: user.role_associated_resource_ids,
-    };
-  } else if (userProfile) {
-    activeUserInfo = {
-      disabled: false,
-      email: userProfile.email,
-      fullName: userProfile.name,
-      role: ROLE.UNREGISTERED,
-      roleAssociatedResourceIds: [],
-    };
-  } else {
-    res.status(401).json({ message: "Not authenticated" });
-    return;
+  let user = await getUserFromAuthorization(req.headers.authorization);
+  if (!user) {
+    if (userProfile) {
+      user = await createUser({
+        disabled: false,
+        email: userProfile.email,
+        fullName: userProfile.name,
+        role: ROLE.STAKEHOLDER,
+        roleAssociatedResourceIds: [],
+      });
+    } else {
+      throw new UnauthenticatedError("Not authenticated");
+    }
   }
+  await updateLastLogin(user.id);
+  const activeUserInfo: HCAAtlasTrackerActiveUser = {
+    disabled: user.disabled,
+    email: user.email,
+    fullName: user.full_name,
+    role: user.role,
+    roleAssociatedResourceIds: user.role_associated_resource_ids,
+  };
   res.json(activeUserInfo);
 });
