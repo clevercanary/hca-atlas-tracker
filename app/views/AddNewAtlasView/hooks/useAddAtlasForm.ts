4b605963933d01818c8fed82f4e9c83da47f6fd4
hunterckx <118154470+hunterckx@users.noreply.github.com>
2024-12-15 20:44:45 -0800

    feat: map between atlas doi input and api data (#485)

--- app/views/AddNewAtlasView/hooks/useAddAtlasForm.ts
+++ app/views/AddNewAtlasView/hooks/useAddAtlasForm.ts
@@ -1,4 +1,5 @@
 import { HCAAtlasTrackerAtlas } from "../../../apis/catalog/hca-atlas-tracker/common/entities";
+import { NewAtlasData as APINewAtlasData } from "../../../apis/catalog/hca-atlas-tracker/common/schema";
 import { FormMethod } from "../../../hooks/useForm/common/entities";
 import { useForm } from "../../../hooks/useForm/useForm";
 import { NewAtlasData } from "../common/entities";
@@ -13,7 +14,8 @@ export const useAddAtlasForm = (): FormMethod<
   return useForm<NewAtlasData, HCAAtlasTrackerAtlas>(
     SCHEMA,
     undefined,
-    mapSchemaValues
+    mapSchemaValues,
+    mapApiValues
   );
 };
 
@@ -29,5 +31,18 @@ function mapSchemaValues(): Partial<NewAtlasData> {
         name: "",
       },
     ],
+  };
+}
+
+/**
+ * Returns API payload mapped from data.
+ * @param data - Form data.
+ * @param data.doi - DOI.
+ * @returns API payload.
+ */
+function mapApiValues({ doi, ...data }: NewAtlasData): APINewAtlasData {
+  return {
+    ...data,
+    dois: doi ? [doi] : undefined,
   };
 }
