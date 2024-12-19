ec965354019f5b4f179627272a372b05be1fd208
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-12-19 16:29:01 +1000

    feat: add instructions to the atlas detail tabs (#502)

--- app/components/common/MDXContent/index.tsx
+++ app/components/common/MDXContent/index.tsx
@@ -1,3 +1,4 @@
 export { default as ConfidentialNotice } from "./confidentialNotice.mdx";
 export { default as LoginTermsOfService } from "./loginTermsOfService.mdx";
 export { default as LoginText } from "./loginText.mdx";
+export { default as SourceDatasetsInformation } from "./sourceDatasetsInformation.mdx";
