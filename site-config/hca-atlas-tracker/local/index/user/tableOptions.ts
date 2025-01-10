c4d8e826c7023924206a4f829676f30f69d06b7f
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2025-01-09 16:56:41 -0800

    feat: allow grouping users (#520) (#528)

--- site-config/hca-atlas-tracker/local/index/user/tableOptions.ts
+++ site-config/hca-atlas-tracker/local/index/user/tableOptions.ts
@@ -6,8 +6,10 @@ import { HCAAtlasTrackerUser } from "../../../../../app/apis/catalog/hca-atlas-t
 import { HCA_ATLAS_TRACKER_CATEGORY_KEY } from "../../../category";
 
 export const TABLE_OPTIONS: ListConfig<HCAAtlasTrackerUser>["tableOptions"] = {
+  enableGrouping: true,
   enableRowPosition: true,
   initialState: {
+    expanded: true,
     sorting: [
       {
         desc: SORT_DIRECTION.ASCENDING,
