f250dd1607cc5ec43d3a3c236b109fb65012bec5
Jonah Paten <jonah@clevercanary.com>
2025-01-19 23:28:10 -0800

    feat: added roadmap grouping by default (#546) (#551)
    * feat: added roadmap grouping by default (#546)
    
    * fix: deleted atlas roadmap shared filter (#546)
    
    * fix: added target_completion_date to default sort (#546)

--- site-config/hca-atlas-tracker/local/index/atlas/tableOptions.ts
+++ site-config/hca-atlas-tracker/local/index/atlas/tableOptions.ts
@@ -19,7 +19,16 @@ export const TABLE_OPTIONS: ListConfig<HCAAtlasTrackerListAtlas>["tableOptions"]
         [HCA_ATLAS_TRACKER_CATEGORY_KEY.COMPONENT_ATLAS_COUNT]: false,
       },
       expanded: true,
+      grouping: [HCA_ATLAS_TRACKER_CATEGORY_KEY.TARGET_COMPLETION_DATE],
       sorting: [
+        {
+          desc: SORT_DIRECTION.ASCENDING,
+          id: HCA_ATLAS_TRACKER_CATEGORY_KEY.TARGET_COMPLETION_DATE,
+        },
+        {
+          desc: SORT_DIRECTION.ASCENDING,
+          id: HCA_ATLAS_TRACKER_CATEGORY_KEY.PUBLICATION_STATUS,
+        },
         {
           desc: SORT_DIRECTION.ASCENDING,
           id: HCA_ATLAS_TRACKER_CATEGORY_KEY.NAME,
