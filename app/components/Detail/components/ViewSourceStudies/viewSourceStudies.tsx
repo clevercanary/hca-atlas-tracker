5d98ba0973f35118c20fd7064995d9cd385c2652
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-02-26 14:35:43 -0800

    feat: update source study hca status labels (#596)

--- app/components/Detail/components/ViewSourceStudies/viewSourceStudies.tsx
+++ app/components/Detail/components/ViewSourceStudies/viewSourceStudies.tsx
@@ -86,7 +86,7 @@ export const ViewSourceStudies = ({
                 pathParameter,
                 atlasLinkedDatasetCountsByStudyId
               )}
-              gridTemplateColumns="max-content minmax(260px, 1fr) minmax(100px, 118px) minmax(80px, 130px) repeat(2, minmax(130px, 150px)) minmax(195px, 210px)"
+              gridTemplateColumns="max-content minmax(260px, 1fr) minmax(100px, 118px) minmax(80px, 130px) repeat(2, minmax(130px, 150px)) minmax(170px, 185px)"
               items={sortedSourceStudies}
               tableOptions={TABLE_OPTIONS}
             />
