0b4450a44652a7fb2afaf1eae2c5c4668226be11
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-03-04 15:16:36 -0800

    refactor: update initialization of auth providers (#395)

--- site-config/hca-atlas-tracker/local/authentication/next-auth.ts
+++ site-config/hca-atlas-tracker/local/authentication/next-auth.ts
@@ -1,7 +1,11 @@
 import { NextAuthOptions } from "next-auth";
-import AzureAdProvider from "next-auth/providers/azure-ad";
 import GoogleProvider from "next-auth/providers/google";
 import { SESSION_MAX_AGE } from "./constants";
+import { GoogleAuthParams } from "./entities";
+
+const googleAuthParams: GoogleAuthParams = JSON.parse(
+  process.env.GOOGLE_AUTH ?? ""
+);
 
 export const nextAuthOptions: NextAuthOptions = {
   debug: false,
@@ -10,13 +14,8 @@ export const nextAuthOptions: NextAuthOptions = {
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID ?? "",
-      clientSecret: process.env.GOOGLE_SECRET ?? "",
-    }),
-    AzureAdProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID ?? "",
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET ?? "",
-      tenantId: process.env.AZURE_AD_TENANT_ID,
+      clientId: googleAuthParams.client_id,
+      clientSecret: googleAuthParams.client_secret,
     }),
   ],
   session: {
