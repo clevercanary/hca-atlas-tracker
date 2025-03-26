c4d64c6b4cf79aeebc5e8b6f1b4a8ce420cbc7b1
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2025-03-25 21:07:26 -0700

    chore: delete unused `files-outdated` folder (#622) (#632)

--- files-outdated/constants.ts
+++
@@ -1,9 +0,0 @@
-export const DATA_SOURCE = {
-  defaultListParams: {
-    size: "100",
-  },
-  defaultParams: {
-    catalog: "dcp35",
-  },
-  url: "https://service.azul.data.humancellatlas.org/index",
-};
