862e5d6a70234d2ec0edb96fb0cc85c7f08baa93
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2025-05-31 15:29:05 -0700

    fix: accept project urls for cap id rather than dataset urls (#649) (#653)

--- app/components/Form/components/Input/components/CapId/common/constants.ts
+++
@@ -1,5 +0,0 @@
-import { escapeRegExp } from "@databiosphere/findable-ui/lib/common/utils";
-
-export const CAP_ID_REGEXP = new RegExp(
-  `^(?:${escapeRegExp("https://celltype.info/project/")}\\d+/dataset/\\d+)?$`
-);
--- app/views/SourceStudyView/common/schema.ts
+++ app/views/SourceStudyView/common/schema.ts
@@ -1,17 +1,19 @@
 import { object, string } from "yup";
-import { metadataSpreadsheetUrlsSchema } from "../../../apis/catalog/hca-atlas-tracker/common/schema";
+import {
+  CAP_PROJECT_URL_REGEXP,
+  metadataSpreadsheetUrlsSchema,
+} from "../../../apis/catalog/hca-atlas-tracker/common/schema";
 import {
   CELLXGENE_COLLECTION_ID_REGEX,
   HCA_PROJECT_ID_REGEX,
 } from "../../../common/constants";
-import { CAP_ID_REGEXP } from "../../../components/Form/components/Input/components/CapId/common/constants";
 import { newSourceStudySchema } from "../../AddNewSourceStudyView/common/schema";
 import { FIELD_NAME } from "./constants";
 
 export const sourceStudyEditSchema = newSourceStudySchema.concat(
   object({
     [FIELD_NAME.CAP_ID]: string()
-      .matches(CAP_ID_REGEXP, "Invalid CAP ID")
+      .matches(CAP_PROJECT_URL_REGEXP, "Invalid CAP ID")
       .default("")
       .notRequired(),
     [FIELD_NAME.CELLXGENE_COLLECTION_ID]: string()
