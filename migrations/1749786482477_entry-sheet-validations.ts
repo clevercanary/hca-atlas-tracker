d4bd662d201631d2f937eb0de597c77843a07c43
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-06-12 21:28:59 -0700

    feat: add entry sheet validations table (#655)

---
+++ migrations/1749786482477_entry-sheet-validations.ts
@@ -0,0 +1,47 @@
+import { MigrationBuilder } from "node-pg-migrate";
+
+export const up = (pgm: MigrationBuilder): void => {
+  pgm.createTable(
+    { name: "entry_sheet_validations", schema: "hat" },
+    {
+      atlas_id: {
+        notNull: true,
+        references: { name: "atlases", schema: "hat" },
+        type: "uuid",
+      },
+      entry_sheet_id: {
+        notNull: true,
+        type: "text",
+        unique: true,
+      },
+      entry_sheet_title: {
+        type: "text",
+      },
+      id: {
+        default: pgm.func("gen_random_uuid ()"),
+        notNull: true,
+        primaryKey: true,
+        type: "uuid",
+      },
+      last_synced: {
+        notNull: true,
+        type: "timestamp",
+      },
+      last_updated: {
+        type: "jsonb",
+      },
+      source_study_id: {
+        notNull: true,
+        references: { name: "source_studies", schema: "hat" },
+        type: "uuid",
+      },
+      validation_report: {
+        notNull: true,
+        type: "jsonb",
+      },
+      validation_summary: {
+        type: "jsonb",
+      },
+    }
+  );
+};
