4cad403d68b41d49c59c6253d9fc50e6e3796050
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-07-11 15:53:48 +1000

    feat: move sync metadata entry sheets button to the metadata entry sheets tab (#696)

---
+++ app/views/AtlasMetadataEntrySheetsView/hooks/UseEntrySheetSync/hook.ts
@@ -0,0 +1,21 @@
+import { useCallback, useState } from "react";
+import { PathParameter } from "../../../../common/entities";
+import { EntrySheetSyncState, UseEntrySheetSync } from "./types";
+import { startEntrySheetSync } from "./utils";
+
+export const useEntrySheetSync = (
+  pathParameter: PathParameter
+): UseEntrySheetSync => {
+  const [entrySheetSyncState, setEntrySheetSyncState] =
+    useState<EntrySheetSyncState>({ started: false });
+
+  const onSyncEntrySheets = useCallback(() => {
+    setEntrySheetSyncState({ started: true });
+    startEntrySheetSync(pathParameter).catch((error: unknown) => {
+      setEntrySheetSyncState({ error, started: true });
+      throw error;
+    });
+  }, [pathParameter]);
+
+  return { entrySheetSyncState, onSyncEntrySheets };
+};
--- app/views/AtlasView/atlasView.tsx
+++ app/views/AtlasView/atlasView.tsx
@@ -1,10 +1,7 @@
 import { ConditionalComponent } from "@databiosphere/findable-ui/lib/components/ComponentCreator/components/ConditionalComponent/conditionalComponent";
-import { Button } from "@mui/material";
-import { getRequestURL } from "app/common/utils";
-import { Fragment, useCallback, useEffect, useState } from "react";
-import { API } from "../../apis/catalog/hca-atlas-tracker/common/api";
+import { Fragment } from "react";
 import { getAtlasName } from "../../apis/catalog/hca-atlas-tracker/common/utils";
-import { FETCH_STATUS, METHOD, PathParameter } from "../../common/entities";
+import { PathParameter } from "../../common/entities";
 import { AccessPrompt } from "../../components/common/Form/components/FormManager/components/AccessPrompt/accessPrompt";
 import { shouldRenderView } from "../../components/Detail/common/utils";
 import { Breadcrumbs } from "../../components/Detail/components/TrackerForm/components/Breadcrumbs/breadcrumbs";
@@ -22,11 +19,6 @@ interface AtlasViewProps {
   pathParameter: PathParameter;
 }
 
-interface EntrySheetSyncState {
-  error?: unknown;
-  started: boolean;
-}
-
 export const AtlasView = ({ pathParameter }: AtlasViewProps): JSX.Element => {
   const formMethod = useEditAtlasForm(pathParameter);
   const formManager = useEditAtlasFormManager(pathParameter, formMethod);
@@ -36,37 +28,10 @@ export const AtlasView = ({ pathParameter }: AtlasViewProps): JSX.Element => {
     isLoading,
   } = formManager;
   const { data: atlas } = formMethod;
-  const [entrySheetSyncState, setEntrySheetSyncState] =
-    useState<EntrySheetSyncState>({ started: false });
-  const onSyncEntrySheets = useCallback(() => {
-    setEntrySheetSyncState({ started: true });
-    startEntrySheetSync(pathParameter).catch((error: unknown) =>
-      setEntrySheetSyncState({ error, started: true })
-    );
-  }, [pathParameter]);
-  useEffect(() => {
-    if (Object.hasOwn(entrySheetSyncState, "error"))
-      throw entrySheetSyncState.error;
-  }, [entrySheetSyncState]);
   if (isLoading) return <Fragment />;
   return (
     <ConditionalComponent isIn={shouldRenderView(canView, Boolean(atlas))}>
       <DetailView
-        actions={
-          // Simple placeholder sync button -- this and related code should presumably be refactored once the actual entry sheets interface is implemented
-          <div>
-            <Button
-              color="secondary"
-              variant="contained"
-              disabled={entrySheetSyncState.started}
-              onClick={onSyncEntrySheets}
-            >
-              {entrySheetSyncState.started
-                ? "Sync started"
-                : "Sync entry sheets"}
-            </Button>
-          </div>
-        }
         breadcrumbs={
           <Breadcrumbs
             breadcrumbs={getBreadcrumbs(atlas)}
@@ -106,23 +71,4 @@ function renderAccessFallback(formManager: FormManager): JSX.Element | null {
   } = formManager;
   if (!canView) return <AccessPrompt text="to view the atlas" />;
   return null;
-}
-
-async function startEntrySheetSync(
-  pathParameter: PathParameter
-): Promise<void> {
-  const res = await fetch(
-    getRequestURL(API.ATLAS_ENTRY_SHEETS_SYNC, pathParameter),
-    { method: METHOD.POST }
-  );
-  if (res.status !== FETCH_STATUS.ACCEPTED) {
-    const responseText = await res.text();
-    let responseError: unknown;
-    try {
-      responseError = new Error(JSON.parse(responseText).message);
-    } finally {
-      if (!responseError) responseError = new Error(responseText);
-    }
-    throw responseError;
-  }
 }
