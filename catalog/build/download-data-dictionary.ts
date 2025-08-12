ea85e8a3e7a23dde7ae815df2240e6df43ae2eb9
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-08-11 23:49:29 -0700

    feat: update download script to get latest data dictionary from validation tools main (#728)

--- catalog/build/download-data-dictionary.ts
+++ catalog/build/download-data-dictionary.ts
@@ -1,6 +1,6 @@
 import fsp from "fs/promises";
 
-const DATA_DICTIONARY_URL = "https://raw.githubusercontent.com/clevercanary/hca-validation-tools/05525826b42452a51c7649996a75fb27f64fdd7b/data_dictionaries/core_data_dictionary.json";
+const DATA_DICTIONARY_URL = "https://raw.githubusercontent.com/clevercanary/hca-validation-tools/refs/heads/main/data_dictionaries/core_data_dictionary.json";
 const DATA_DICTIONARY_DOWNLOAD_PATH = "./catalog/downloaded/data-dictionary.json";
 
 
