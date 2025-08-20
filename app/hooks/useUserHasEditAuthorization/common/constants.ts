455b3c3c3e60c8002dae0a51572754c7ae1c2f57
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-08-20 14:49:02 +1000

    feat: remove the 'add integration object' button from the component atlases page (#749)

--- app/hooks/useUserHasEditAuthorization/common/constants.ts
+++ app/hooks/useUserHasEditAuthorization/common/constants.ts
@@ -4,7 +4,6 @@ import { RouteValue } from "../../../routes/entities";
 export const ROUTES: RouteValue[] = [
   ROUTE.COMPONENT_ATLAS,
   ROUTE.COMPONENT_ATLASES,
-  ROUTE.CREATE_COMPONENT_ATLAS,
   ROUTE.CREATE_SOURCE_STUDY,
   ROUTE.SOURCE_DATASETS,
   ROUTE.SOURCE_STUDIES,
