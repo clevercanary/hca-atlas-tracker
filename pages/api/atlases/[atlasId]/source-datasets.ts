3affa61d4ac079887cbb67876df188fe0db9911d
Dave Rogers <dave@clevercanary.com>
2025-09-02 19:10:01 -0700

    refactor: constants for init files fileToComponentAtlasMap (#790)

--- pages/api/atlases/[atlasId]/source-datasets.ts
+++ pages/api/atlases/[atlasId]/source-datasets.ts
@@ -4,6 +4,11 @@ import { METHOD } from "../../../../app/common/entities";
 import { getAtlasDatasets } from "../../../../app/services/source-datasets";
 import { handler, method, role } from "../../../../app/utils/api-handler";
 
+/**
+ * Get all source datasets linked to the given atlas.
+ * @param req - Next API request.
+ * @param res - Next API response.
+ */
 export default handler(
   method(METHOD.GET),
   role(ROLE_GROUP.READ),
