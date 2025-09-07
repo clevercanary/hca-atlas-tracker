5e4ba7ecf42319278c7f89804fe306be9b488f80
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-09-06 18:50:10 -0700

    feat: add api to sync files from s3 (#801)

---
+++ pages/api/sync-files.ts
@@ -0,0 +1,16 @@
+import { syncFilesFromS3 } from "app/services/s3-sync";
+import { ROLE } from "../../app/apis/catalog/hca-atlas-tracker/common/entities";
+import { METHOD } from "../../app/common/entities";
+import { handler, method, role } from "../../app/utils/api-handler";
+
+export default handler(
+  method(METHOD.POST),
+  role(ROLE.CONTENT_ADMIN),
+  async (req, res) => {
+    syncFilesFromS3().catch((e) => {
+      console.error("Encountered error while syncing from S3");
+      console.error(e);
+    });
+    res.status(202).end();
+  }
+);
