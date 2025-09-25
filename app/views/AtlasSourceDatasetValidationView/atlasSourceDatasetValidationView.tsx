16feed824f4ca1592cc70922eb3518c61ee32406
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-09-26 09:28:43 +1000

    feat: updated conditional rendering rules (#875)

--- app/views/AtlasSourceDatasetValidationView/atlasSourceDatasetValidationView.tsx
+++ app/views/AtlasSourceDatasetValidationView/atlasSourceDatasetValidationView.tsx
@@ -1,9 +1,12 @@
 import { ConditionalComponent } from "@databiosphere/findable-ui/lib/components/ComponentCreator/components/ConditionalComponent/conditionalComponent";
+import { Fragment } from "react";
 import { getRouteURL } from "../../common/utils";
+import { shouldRenderView } from "../../components/Detail/common/utils";
 import { Breadcrumbs } from "../../components/Detail/components/TrackerForm/components/Breadcrumbs/breadcrumbs";
 import { Tabs } from "../../components/Entity/components/common/Tabs/tabs";
 import { DetailView } from "../../components/Layout/components/Detail/detailView";
 import { useFetchAtlas } from "../../hooks/useFetchAtlas";
+import { useFormManager } from "../../hooks/useFormManager/useFormManager";
 import { ROUTE } from "../../routes/constants";
 import { useFetchAtlasSourceDataset } from "../AtlasSourceDatasetView/hooks/useFetchAtlasSourceDataset";
 import { getBreadcrumbs, getTabs } from "./common/utils";
@@ -14,8 +17,16 @@ export const AtlasSourceDatasetValidationView = ({
 }: Props): JSX.Element => {
   const { atlas } = useFetchAtlas(pathParameter);
   const { sourceDataset } = useFetchAtlasSourceDataset(pathParameter);
+  const formManager = useFormManager();
+  const {
+    access: { canView },
+    isLoading,
+  } = formManager;
+  if (isLoading) return <Fragment />;
   return (
-    <ConditionalComponent isIn={true}>
+    <ConditionalComponent
+      isIn={shouldRenderView(canView, Boolean(atlas && sourceDataset))}
+    >
       <DetailView
         backPath={getRouteURL(ROUTE.ATLAS_SOURCE_DATASET, pathParameter)}
         breadcrumbs={
