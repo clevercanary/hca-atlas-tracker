6f899daacb7c8e6188f953b0d1dd821c51793042
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-09-26 01:53:26 -0700

    feat: add function to get a download url for an s3 key (#873)

---
+++ app/services/s3-operations.ts
@@ -0,0 +1,24 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3 = new S3Client();
+
+function getBucket(): string {
+  const bucket = process.env.DOWNLOAD_BUCKET;
+  if (!bucket)
+    throw new Error("S3 bucket for downloads not specified in environment");
+  return bucket;
+}
+
+export async function getDownloadUrl(key: string): Promise<string> {
+  const bucket = getBucket();
+
+  const command = new GetObjectCommand({
+    Bucket: bucket,
+    Key: key,
+    // Force browser download instead of inline display
+    ResponseContentDisposition: "attachment",
+  });
+
+  return await getSignedUrl(s3, command, { expiresIn: 172800 }); // 48 hours
+}
