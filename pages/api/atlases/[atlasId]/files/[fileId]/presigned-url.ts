885371101656f5b14ecf6024bbf74950b0fde2b0
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-09-27 16:59:40 -0700

    feat: add api to get a download url for a file (#873)

---
+++ pages/api/atlases/[atlasId]/files/[fileId]/presigned-url.ts
@@ -0,0 +1,23 @@
+import { ROLE } from "../../../../../../app/apis/catalog/hca-atlas-tracker/common/entities";
+import { METHOD } from "../../../../../../app/common/entities";
+import { getAtlasFileDownloadUrl } from "../../../../../../app/services/files";
+import {
+  handler,
+  integrationLeadAssociatedAtlasOnly,
+  method,
+  role,
+} from "../../../../../../app/utils/api-handler";
+
+/**
+ * API route for getting a presigned download URL for a given file of a given atlas.
+ */
+export default handler(
+  method(METHOD.POST),
+  role([ROLE.CONTENT_ADMIN, ROLE.INTEGRATION_LEAD]),
+  integrationLeadAssociatedAtlasOnly,
+  async (req, res) => {
+    const atlasId = req.query.atlasId as string;
+    const fileId = req.query.fileId as string;
+    res.status(200).send(await getAtlasFileDownloadUrl(atlasId, fileId));
+  }
+);
