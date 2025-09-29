3bf6dcb9df18d392d6ccfb69fbb52e8c601a18dd
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-09-29 14:38:04 +1000

    feat: update code styles (#868)

--- app/components/Entity/components/common/Table/components/TableCell/components/FileDownloadCell/components/Dialog/components/DialogContent/components/Section/components/Code/code.styles.ts
+++ app/components/Entity/components/common/Table/components/TableCell/components/FileDownloadCell/components/Dialog/components/DialogContent/components/Section/components/Code/code.styles.ts
@@ -6,8 +6,6 @@ export const StyledCode = styled(Code)`
   border: 1px solid ${PALETTE.INFO_LIGHT};
   flex: none;
   margin: -4px 0 0 0;
-  max-height: 72px;
-  overflow: hidden;
   padding: 8px 16px;
 
   &::after {
