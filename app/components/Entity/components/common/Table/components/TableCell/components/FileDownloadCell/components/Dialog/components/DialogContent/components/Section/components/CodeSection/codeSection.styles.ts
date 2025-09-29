71c6c8e64f9db1fd94402add2d2e4aad7a4a9f29
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-09-29 14:51:33 +1000

    feat: updated code section to display title, code, and timeline info (#868)

--- app/components/Entity/components/common/Table/components/TableCell/components/FileDownloadCell/components/Dialog/components/DialogContent/components/Section/components/CodeSection/codeSection.styles.ts
+++ app/components/Entity/components/common/Table/components/TableCell/components/FileDownloadCell/components/Dialog/components/DialogContent/components/Section/components/CodeSection/codeSection.styles.ts
@@ -5,7 +5,7 @@ import styled from "@emotion/styled";
 export const StyledCode = styled(Code)`
   border: 1px solid ${PALETTE.INFO_LIGHT};
   flex: none;
-  margin: -4px 0 0 0;
+  margin: 0;
   padding: 8px 16px;
 
   &::after {
