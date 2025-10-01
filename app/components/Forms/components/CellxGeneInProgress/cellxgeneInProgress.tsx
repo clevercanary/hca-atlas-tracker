eba257d473e5eefe275a3f269e3482c39f1212b2
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-01 12:31:19 +1000

    refactor: cellxgeneinprogress (#908)

--- app/components/Forms/components/CellxGeneInProgress/cellxgeneInProgress.tsx
+++ app/components/Forms/components/CellxGeneInProgress/cellxgeneInProgress.tsx
@@ -1,6 +1,5 @@
-import { ButtonPrimary } from "@databiosphere/findable-ui/lib/components/common/Button/components/ButtonPrimary/buttonPrimary";
 import { BUTTON_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/button";
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { useCallback, useRef, useState } from "react";
 import {
   TASK_STATUS,
@@ -62,13 +61,15 @@ export const CellxGeneInProgressForm = (): JSX.Element => {
         />
       </div>
       <div>
-        <ButtonPrimary
+        <Button
+          color={BUTTON_PROPS.COLOR.PRIMARY}
           disabled={isDisabled}
           onClick={onSave}
           size={BUTTON_PROPS.SIZE.SMALL}
+          variant={BUTTON_PROPS.VARIANT.CONTAINED}
         >
           Update
-        </ButtonPrimary>
+        </Button>
       </div>
       {responseErrors || updateResult ? (
         <>
