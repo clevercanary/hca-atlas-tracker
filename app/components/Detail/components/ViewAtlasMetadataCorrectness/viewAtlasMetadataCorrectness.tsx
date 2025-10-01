46d7ba253427d6729b38765e6cca2f5b334cc8d7
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-01 10:42:03 +1000

    refactor: config (#908)

--- app/components/Detail/components/ViewAtlasMetadataCorrectness/viewAtlasMetadataCorrectness.tsx
+++ app/components/Detail/components/ViewAtlasMetadataCorrectness/viewAtlasMetadataCorrectness.tsx
@@ -1,6 +1,6 @@
 import { SectionTitle } from "@databiosphere/findable-ui/lib/components/common/Section/components/SectionTitle/sectionTitle";
 import { Link } from "@databiosphere/findable-ui/lib/components/Links/components/Link/link";
-import { TEXT_BODY_400 } from "@databiosphere/findable-ui/lib/theme/common/typography";
+import { TYPOGRAPHY_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/typography";
 import { Typography } from "@mui/material";
 import { HCAAtlasTrackerAtlas } from "app/apis/catalog/hca-atlas-tracker/common/entities";
 import { FormManager } from "../../../../hooks/useFormManager/common/entities";
@@ -23,7 +23,7 @@ export const ViewAtlasMetadataCorrectness = ({
   return (
     <SectionPaper elevation={0}>
       <SectionTitle title="Metadata Correctness Report" />
-      <Typography variant={TEXT_BODY_400}>
+      <Typography variant={TYPOGRAPHY_PROPS.VARIANT.BODY_400}>
         {atlas?.metadataCorrectnessUrl ? (
           <Link url={atlas.metadataCorrectnessUrl} label="View Report" />
         ) : (
