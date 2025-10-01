ed3aafcfc8c46ef05eb24d925018eee7dc530fe3
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-01 11:41:31 +1000

    feat: update section mui prop constants (#908)

--- app/components/Detail/components/TrackerForm/components/Section/components/ListSection/constants.ts
+++ app/components/Detail/components/TrackerForm/components/Section/components/ListSection/constants.ts
@@ -1,16 +1,19 @@
+import { BUTTON_PROPS as MUI_BUTTON_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/button";
+import { ICON_BUTTON_PROPS as MUI_ICON_BUTTON_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/iconButton";
+import { SVG_ICON_PROPS as MUI_SVG_ICON_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/svgIcon";
 import { ButtonProps, IconButtonProps, SvgIconProps } from "@mui/material";
 
 export const BUTTON_PROPS: Partial<ButtonProps> = {
-  color: "secondary",
-  variant: "contained",
+  color: MUI_BUTTON_PROPS.COLOR.SECONDARY,
+  variant: MUI_BUTTON_PROPS.VARIANT.CONTAINED,
 };
 
 export const ICON_BUTTON_PROPS: Partial<IconButtonProps> = {
-  color: "secondary",
-  size: "large",
+  color: MUI_ICON_BUTTON_PROPS.COLOR.SECONDARY,
+  size: MUI_ICON_BUTTON_PROPS.SIZE.LARGE,
 };
 
 export const SVG_ICON_PROPS: Partial<SvgIconProps> = {
-  color: "inkLight",
-  fontSize: "small",
+  color: MUI_SVG_ICON_PROPS.COLOR.INK_LIGHT,
+  fontSize: MUI_SVG_ICON_PROPS.FONT_SIZE.SMALL,
 };
