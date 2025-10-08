82d8debbf29bf06d2c711a5096df8826ec6c4442
Fran McDade <frano-m@users.noreply.github.com>
2025-10-08 13:56:04 +1000

    feat: increase the session inactivity timeout to 1 hour (#472) (#925)
    Co-authored-by: Fran McDade <18710366+frano-m@users.noreply.github.com>

--- site-config/hca-atlas-tracker/local/authentication/constants.ts
+++ site-config/hca-atlas-tracker/local/authentication/constants.ts
@@ -1,3 +1,3 @@
-export const SESSION_TIMEOUT = 15 * 60 * 1000; // 15 minutes
+export const SESSION_TIMEOUT = 60 * 60 * 1000; // 1 hour
 export const SESSION_MAX_AGE = 5 * 60 * 1000; // 5 minutes
 export const SESSION_REFETCH_INTERVAL = 4 * 60 * 1000; // 4 minutes
