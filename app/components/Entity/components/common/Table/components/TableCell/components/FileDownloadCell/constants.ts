4bcfa6e2b5e3a6e14b9c6799a8f8fe79b112cd1b
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-09 16:21:00 +1000

    feat: updated file download styles (#922)

--- app/components/Entity/components/common/Table/components/TableCell/components/FileDownloadCell/constants.ts
+++ app/components/Entity/components/common/Table/components/TableCell/components/FileDownloadCell/constants.ts
@@ -1,7 +1,13 @@
 import { ICON_BUTTON_PROPS as MUI_ICON_BUTTON_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/iconButton";
-import { IconButtonProps } from "@mui/material";
+import { SVG_ICON_PROPS as MUI_SVG_ICON_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/svgIcon";
+import { IconButtonProps, SvgIconProps } from "@mui/material";
 
 export const ICON_BUTTON_PROPS: IconButtonProps = {
-  color: MUI_ICON_BUTTON_PROPS.COLOR.PRIMARY,
+  color: MUI_ICON_BUTTON_PROPS.COLOR.SECONDARY,
   size: MUI_ICON_BUTTON_PROPS.SIZE.MEDIUM,
+};
+
+export const SVG_ICON_PROPS: SvgIconProps = {
+  color: MUI_SVG_ICON_PROPS.COLOR.INK_LIGHT,
+  fontSize: MUI_SVG_ICON_PROPS.FONT_SIZE.SMALL,
 };
