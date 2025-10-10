633da71997695504349609e93e9f331ddf9ccc3c
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-10 11:38:56 +1000

    feat: added file archive edit button to sd toolbar (#922)

---
+++ app/hooks/UseEditFileArchived/hook.ts
@@ -0,0 +1,29 @@
+import { useCallback } from "react";
+import { METHOD } from "../../common/entities";
+import { fetchResource, isFetchStatusOk } from "../../common/utils";
+import { OnSubmitOptions, Payload, UseEditFileArchived } from "./entities";
+
+export const useEditFileArchived = (): UseEditFileArchived => {
+  const onSubmit = useCallback(
+    async (
+      requestURL: string,
+      payload: Payload,
+      options?: OnSubmitOptions
+    ): Promise<void> => {
+      const res = await fetchResource(requestURL, METHOD.PATCH, payload);
+      if (isFetchStatusOk(res.status)) {
+        options?.onSuccess?.();
+      } else {
+        throw new Error(
+          await res
+            .json()
+            .then(({ message }) => message)
+            .catch(() => `Received ${res.status} response`)
+        );
+      }
+    },
+    []
+  );
+
+  return { onSubmit };
+};
