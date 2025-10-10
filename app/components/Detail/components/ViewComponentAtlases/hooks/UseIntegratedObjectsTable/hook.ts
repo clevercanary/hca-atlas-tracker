179f2697497bbabbfcc7e976b6d10fd1f938bdc5
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-10 13:56:36 +1000

    feat: update archived table columns with reduced set of columns (#922)

--- app/components/Detail/components/ViewComponentAtlases/hooks/UseIntegratedObjectsTable/hook.ts
+++ app/components/Detail/components/ViewComponentAtlases/hooks/UseIntegratedObjectsTable/hook.ts
@@ -3,14 +3,17 @@ import { useReactTable } from "@tanstack/react-table";
 import { useEntity } from "../../../../../../providers/entity/hook";
 import { getAtlasComponentAtlasesTableColumns } from "../../../../../../viewModelBuilders/catalog/hca-atlas-tracker/common/viewModelBuilders";
 import { EntityData } from "../../../../../../views/ComponentAtlasesView/entities";
+import { useArchivedState } from "../../../../../Entity/providers/archived/hook";
 import { CORE_OPTIONS } from "../../../../../Table/options/core/constants";
 import { UseIntegratedObjectsTable } from "./entities";
 
 export const useIntegratedObjectsTable = (): UseIntegratedObjectsTable => {
+  const { archivedState } = useArchivedState();
   const { data, formManager } = useEntity();
   const { integratedObjects = [] } = data as EntityData;
   const { access } = formManager || {};
   const { canEdit = false } = access || {};
+  const { archived } = archivedState;
 
   const table = useReactTable({
     columns: getAtlasComponentAtlasesTableColumns(),
@@ -19,16 +22,17 @@ export const useIntegratedObjectsTable = (): UseIntegratedObjectsTable => {
     enableMultiRowSelection: canEdit,
     enableRowSelection: canEdit,
     getRowId: (row) => row.id,
-    initialState: {
+    meta: { canEdit },
+    state: {
       columnVisibility: {
         [COLUMN_IDENTIFIER.ROW_POSITION]: !canEdit,
         [COLUMN_IDENTIFIER.ROW_SELECTION]: canEdit,
         atlasId: false,
         fileId: false,
         id: false,
+        validationStatus: !archived,
       },
     },
-    meta: { canEdit },
   });
 
   return { access, table };
