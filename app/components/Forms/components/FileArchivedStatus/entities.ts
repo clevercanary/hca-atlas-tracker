a7de226ae2135172fdcb2f42a0f1002c156334d9
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-10 14:59:09 +1000

    feat: added archive toggle to source dataset (#922)

--- app/components/Forms/components/FileArchivedStatus/entities.ts
+++ app/components/Forms/components/FileArchivedStatus/entities.ts
@@ -1,9 +1,10 @@
+import { BaseComponentProps } from "@databiosphere/findable-ui/lib/components/types";
 import {
   OnSubmitOptions,
   Payload,
 } from "../../../../hooks/UseEditFileArchived/entities";
 
-export interface Props {
+export interface Props extends BaseComponentProps {
   isArchived: boolean;
   payload: Payload;
   submitOptions?: OnSubmitOptions;
--- app/views/AtlasSourceDatasetView/atlasSourceDatasetView.tsx
+++ app/views/AtlasSourceDatasetView/atlasSourceDatasetView.tsx
@@ -1,14 +1,20 @@
 import { ConditionalComponent } from "@databiosphere/findable-ui/lib/components/ComponentCreator/components/ConditionalComponent/conditionalComponent";
 import { Fragment } from "react";
+import { HCAAtlasTrackerSourceDataset } from "../../apis/catalog/hca-atlas-tracker/common/entities";
 import { PathParameter } from "../../common/entities";
 import { AccessPrompt } from "../../components/common/Form/components/FormManager/components/AccessPrompt/accessPrompt";
 import { shouldRenderView } from "../../components/Detail/common/utils";
 import { Breadcrumbs } from "../../components/Detail/components/TrackerForm/components/Breadcrumbs/breadcrumbs";
 import { Tabs } from "../../components/Entity/components/common/Tabs/tabs";
 import { DetailView } from "../../components/Layout/components/Detail/detailView";
+import { Payload } from "../../hooks/UseEditFileArchived/entities";
 import { useFetchAtlas } from "../../hooks/useFetchAtlas";
+import { useFetchDataState } from "../../hooks/useFetchDataState";
 import { FormManager } from "../../hooks/useFormManager/common/entities";
 import { useFormManager } from "../../hooks/useFormManager/useFormManager";
+import { EntityProvider } from "../../providers/entity/provider";
+import { fetchData } from "../../providers/fetchDataState/actions/fetchData/dispatch";
+import { StyledFileArchivedStatus } from "./atlasSourceDatasetView.styles";
 import { VIEW_ATLAS_SOURCE_DATASET_SECTION_CONFIGS } from "./common/sections";
 import { getBreadcrumbs, getTabs } from "./common/utils";
 import { ViewAtlasSourceDataset } from "./components/ViewAtlasSourceDataset/viewAtlasSourceDataset";
@@ -22,38 +28,62 @@ export const AtlasSourceDatasetView = ({
   pathParameter,
 }: AtlasSourceDatasetViewProps): JSX.Element => {
   const { atlas } = useFetchAtlas(pathParameter);
+  const { fetchDataDispatch } = useFetchDataState();
   const formMethod = useEditAtlasSourceDatasetForm(pathParameter);
   const formManager = useFormManager();
   const {
-    access: { canView },
+    access: { canEdit, canView },
     isLoading,
   } = formManager;
   const { data: sourceDataset } = formMethod;
   if (isLoading) return <Fragment />;
   const accessFallback = renderAccessFallback(formManager);
   return (
-    <ConditionalComponent
-      isIn={shouldRenderView(canView, Boolean(atlas && sourceDataset))}
-    >
-      <DetailView
-        breadcrumbs={
-          <Breadcrumbs breadcrumbs={getBreadcrumbs(pathParameter, atlas)} />
-        }
-        mainColumn={
-          <ViewAtlasSourceDataset
-            accessFallback={accessFallback}
-            formManager={formManager}
-            formMethod={formMethod}
-            sectionConfigs={VIEW_ATLAS_SOURCE_DATASET_SECTION_CONFIGS}
-          />
-        }
-        tabs={<Tabs pathParameter={pathParameter} tabs={getTabs()} />}
-        subTitle={sourceDataset?.publicationString}
-        title={sourceDataset?.title || "Source Dataset"}
-      />
-    </ConditionalComponent>
+    <EntityProvider pathParameter={pathParameter}>
+      <ConditionalComponent
+        isIn={shouldRenderView(canView, Boolean(atlas && sourceDataset))}
+      >
+        <DetailView
+          actions={
+            canEdit &&
+            sourceDataset && (
+              <StyledFileArchivedStatus
+                isArchived={sourceDataset.isArchived}
+                payload={mapPayload(sourceDataset)}
+                submitOptions={{
+                  onSuccess: () => fetchDataDispatch(fetchData()),
+                }}
+              />
+            )
+          }
+          breadcrumbs={
+            <Breadcrumbs breadcrumbs={getBreadcrumbs(pathParameter, atlas)} />
+          }
+          mainColumn={
+            <ViewAtlasSourceDataset
+              accessFallback={accessFallback}
+              formManager={formManager}
+              formMethod={formMethod}
+              sectionConfigs={VIEW_ATLAS_SOURCE_DATASET_SECTION_CONFIGS}
+            />
+          }
+          tabs={<Tabs pathParameter={pathParameter} tabs={getTabs()} />}
+          subTitle={sourceDataset?.publicationString}
+          title={sourceDataset?.title || "Source Dataset"}
+        />
+      </ConditionalComponent>
+    </EntityProvider>
   );
 };
+
+/**
+ * Returns the payload for file archiving or unarchiving.
+ * @param sourceDataset - Source dataset.
+ * @returns Payload for file archiving or unarchiving.
+ */
+function mapPayload(sourceDataset: HCAAtlasTrackerSourceDataset): Payload {
+  return { fileIds: [sourceDataset.fileId] };
+}
 
 /**
  * Returns the access fallback component from the form manager access state.
