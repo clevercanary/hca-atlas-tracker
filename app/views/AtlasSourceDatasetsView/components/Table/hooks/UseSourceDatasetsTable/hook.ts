c67401ec1284c4b1766e1a2b593e1bb9ac993751
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-10 13:56:36 +1000

    feat: update archived table columns with reduced set of columns (#922)

--- app/views/AtlasSourceDatasetsView/components/Table/hooks/UseSourceDatasetsTable/hook.ts
+++ app/views/AtlasSourceDatasetsView/components/Table/hooks/UseSourceDatasetsTable/hook.ts
@@ -1,6 +1,7 @@
 import { COLUMN_IDENTIFIER } from "@databiosphere/findable-ui/lib/components/Table/common/columnIdentifier";
 import { SORT_DIRECTION } from "@databiosphere/findable-ui/lib/config/entities";
 import { useReactTable } from "@tanstack/react-table";
+import { useArchivedState } from "../../../../../../components/Entity/providers/archived/hook";
 import { CORE_OPTIONS } from "../../../../../../components/Table/options/core/constants";
 import { SORTING_OPTIONS } from "../../../../../../components/Table/options/sorting/constants";
 import { useEntity } from "../../../../../../providers/entity/hook";
@@ -9,10 +10,12 @@ import { COLUMNS } from "../../columns";
 import { UseSourceDatasetsTable } from "./entities";
 
 export const useSourceDatasetsTable = (): UseSourceDatasetsTable => {
+  const { archivedState } = useArchivedState();
   const { data, formManager } = useEntity();
   const { atlasSourceDatasets = [] } = data as EntityData;
   const { access } = formManager || {};
   const { canEdit = false } = access || {};
+  const { archived } = archivedState;
 
   const table = useReactTable({
     columns: COLUMNS,
@@ -23,17 +26,21 @@ export const useSourceDatasetsTable = (): UseSourceDatasetsTable => {
     enableRowSelection: canEdit,
     getRowId: (row) => row.id,
     initialState: {
-      columnVisibility: {
-        [COLUMN_IDENTIFIER.ROW_POSITION]: !canEdit,
-        [COLUMN_IDENTIFIER.ROW_SELECTION]: canEdit,
-        fileId: false,
-      },
       sorting: [
         { desc: SORT_DIRECTION.ASCENDING, id: "title" },
         { desc: SORT_DIRECTION.ASCENDING, id: "fileName" },
       ],
     },
     meta: { canEdit },
+    state: {
+      columnVisibility: {
+        [COLUMN_IDENTIFIER.ROW_POSITION]: !canEdit,
+        [COLUMN_IDENTIFIER.ROW_SELECTION]: canEdit,
+        fileId: false,
+        reprocessedStatus: !archived,
+        validationStatus: !archived,
+      },
+    },
   });
 
   return { access, table };
