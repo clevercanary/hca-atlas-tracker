01d4832a6f40f82389edabae5200defba71d7391
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-10-10 00:21:42 -0700

    test: add tests for unarchive api (#921)

--- __tests__/api-atlases-id-files-unarchive.test.ts
+++ __tests__/api-atlases-id-files-unarchive.test.ts
@@ -3,31 +3,28 @@ import httpMocks from "node-mocks-http";
 import { FilesSetIsArchivedData } from "../app/apis/catalog/hca-atlas-tracker/common/schema";
 import { METHOD } from "../app/common/entities";
 import { endPgPool } from "../app/services/database";
-import archiveHandler from "../pages/api/atlases/[atlasId]/files/archive";
+import unarchiveHandler from "../pages/api/atlases/[atlasId]/files/unarchive";
 import {
   ATLAS_NONEXISTENT,
   ATLAS_WITH_MISC_SOURCE_STUDIES,
   ATLAS_WITH_MISC_SOURCE_STUDIES_B,
-  COMPONENT_ATLAS_DRAFT_FOO,
-  COMPONENT_ATLAS_MISC_BAR,
-  COMPONENT_ATLAS_MISC_BAZ,
-  COMPONENT_ATLAS_MISC_FOO,
-  COMPONENT_ATLAS_WITH_CELLXGENE_DATASETS,
-  FILE_A_COMPONENT_ATLAS_WITH_MULTIPLE_FILES,
-  FILE_B_COMPONENT_ATLAS_WITH_ARCHIVED_LATEST,
+  COMPONENT_ATLAS_ARCHIVED_BAR,
+  COMPONENT_ATLAS_ARCHIVED_BAZ,
+  COMPONENT_ATLAS_ARCHIVED_FOO,
+  COMPONENT_ATLAS_ARCHIVED_FOOFOO,
+  FILE_B_COMPONENT_ATLAS_WITH_MULTIPLE_FILES,
   FILE_MANIFEST_FOO,
-  SOURCE_DATASET_ATLAS_LINKED_A_BAR,
+  SOURCE_DATASET_ARCHIVED_BAR,
+  SOURCE_DATASET_ARCHIVED_BAZ,
+  SOURCE_DATASET_ARCHIVED_FOO,
+  SOURCE_DATASET_ARCHIVED_FOOBAR,
+  SOURCE_DATASET_ARCHIVED_FOOFOO,
   SOURCE_DATASET_ATLAS_LINKED_A_FOO,
-  SOURCE_DATASET_ATLAS_LINKED_B_BAR,
-  SOURCE_DATASET_ATLAS_LINKED_B_FOO,
-  SOURCE_DATASET_BAR,
-  SOURCE_DATASET_FOO,
-  SOURCE_DATASET_PUBLISHED_WITHOUT_CELLXGENE_ID_FOO,
   STAKEHOLDER_ANALOGOUS_ROLES_WITHOUT_INTEGRATION_LEAD,
   USER_CONTENT_ADMIN,
   USER_DISABLED_CONTENT_ADMIN,
   USER_INTEGRATION_LEAD_PUBLIC,
-  USER_INTEGRATION_LEAD_WITH_MISC_SOURCE_STUDIES,
+  USER_INTEGRATION_LEAD_WITH_MISC_SOURCE_STUDIES_B,
   USER_UNREGISTERED,
 } from "../testing/constants";
 import {
@@ -57,23 +54,23 @@ afterAll(async () => {
   endPgPool();
 });
 
-const ARCHIVE_DATA_SOURCE_DATASET_A_BAR: FilesSetIsArchivedData = {
-  fileIds: [SOURCE_DATASET_ATLAS_LINKED_A_BAR.file.id],
+const UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO: FilesSetIsArchivedData = {
+  fileIds: [SOURCE_DATASET_ARCHIVED_FOO.file.id],
 };
 
-const ARCHIVE_DATA_COMPONENT_ATLAS_MISC_FOO: FilesSetIsArchivedData = {
-  fileIds: [COMPONENT_ATLAS_MISC_FOO.file.id],
+const UNARCHIVE_DATA_COMPONENT_ATLAS_ARCHIVED_FOO: FilesSetIsArchivedData = {
+  fileIds: [COMPONENT_ATLAS_ARCHIVED_FOO.file.id],
 };
 
-const TEST_ROUTE = "/api/atlases/[atlasId]/files/archive";
+const TEST_ROUTE = "/api/atlases/[atlasId]/files/unarchive";
 
 describe(`${TEST_ROUTE}`, () => {
   it("returns error 405 when GET requested", async () => {
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
-          ARCHIVE_DATA_SOURCE_DATASET_A_BAR,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
+          UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO,
           USER_CONTENT_ADMIN,
           METHOD.GET
         )
@@ -84,9 +81,9 @@ describe(`${TEST_ROUTE}`, () => {
   it("returns error 401 when PATCH requested by logged out user", async () => {
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
-          ARCHIVE_DATA_SOURCE_DATASET_A_BAR,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
+          UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO,
           undefined,
           METHOD.PATCH,
           true
@@ -98,9 +95,9 @@ describe(`${TEST_ROUTE}`, () => {
   it("returns error 403 when PATCH requested by unregistered user", async () => {
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
-          ARCHIVE_DATA_SOURCE_DATASET_A_BAR,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
+          UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO,
           USER_UNREGISTERED,
           METHOD.PATCH,
           true
@@ -112,9 +109,9 @@ describe(`${TEST_ROUTE}`, () => {
   it("returns error 403 when PATCH requested by disabled user", async () => {
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
-          ARCHIVE_DATA_SOURCE_DATASET_A_BAR,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
+          UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO,
           USER_DISABLED_CONTENT_ADMIN,
           METHOD.PATCH
         )
@@ -126,11 +123,11 @@ describe(`${TEST_ROUTE}`, () => {
     testApiRole(
       `returns error 403 when PATCH requested`,
       TEST_ROUTE,
-      archiveHandler,
+      unarchiveHandler,
       METHOD.PATCH,
       role,
-      getQueryValues(ATLAS_WITH_MISC_SOURCE_STUDIES.id),
-      ARCHIVE_DATA_SOURCE_DATASET_A_BAR,
+      getQueryValues(ATLAS_WITH_MISC_SOURCE_STUDIES_B.id),
+      UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO,
       false,
       async (res) => {
         expect(res._getStatusCode()).toEqual(403);
@@ -141,9 +138,9 @@ describe(`${TEST_ROUTE}`, () => {
   it("returns error 403 when PATCH requested by user with INTEGRATION_LEAD role for another atlas", async () => {
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
-          ARCHIVE_DATA_SOURCE_DATASET_A_BAR,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
+          UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO,
           USER_INTEGRATION_LEAD_PUBLIC,
           METHOD.PATCH,
           false
@@ -155,9 +152,9 @@ describe(`${TEST_ROUTE}`, () => {
   it("returns error 404 when PATCH requested from nonexistent atlas", async () => {
     expect(
       (
-        await doArchiveRequest(
+        await doUnarchiveRequest(
           ATLAS_NONEXISTENT.id,
-          ARCHIVE_DATA_SOURCE_DATASET_A_BAR,
+          UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO,
           USER_CONTENT_ADMIN,
           METHOD.PATCH,
           true
@@ -169,9 +166,9 @@ describe(`${TEST_ROUTE}`, () => {
   it("returns error 404 when source dataset file is PATCH requested from atlas it doesn't exist on", async () => {
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
-          ARCHIVE_DATA_SOURCE_DATASET_A_BAR,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
+          UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO,
           USER_CONTENT_ADMIN,
           METHOD.PATCH,
           true
@@ -183,9 +180,9 @@ describe(`${TEST_ROUTE}`, () => {
   it("returns error 404 when component atlas file is PATCH requested from atlas it doesn't exist on", async () => {
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
-          ARCHIVE_DATA_COMPONENT_ATLAS_MISC_FOO,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
+          UNARCHIVE_DATA_COMPONENT_ATLAS_ARCHIVED_FOO,
           USER_CONTENT_ADMIN,
           METHOD.PATCH,
           true
@@ -200,8 +197,8 @@ describe(`${TEST_ROUTE}`, () => {
     };
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
           INPUT_DATA,
           USER_CONTENT_ADMIN,
           METHOD.PATCH,
@@ -213,11 +210,11 @@ describe(`${TEST_ROUTE}`, () => {
 
   it("returns error 400 when PATCH requested with non-latest file", async () => {
     const INPUT_DATA: FilesSetIsArchivedData = {
-      fileIds: [FILE_A_COMPONENT_ATLAS_WITH_MULTIPLE_FILES.id],
+      fileIds: [FILE_B_COMPONENT_ATLAS_WITH_MULTIPLE_FILES.id],
     };
     expect(
       (
-        await doArchiveRequest(
+        await doUnarchiveRequest(
           ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
           INPUT_DATA,
           USER_CONTENT_ADMIN,
@@ -228,14 +225,14 @@ describe(`${TEST_ROUTE}`, () => {
     ).toEqual(400);
   });
 
-  it("returns error 400 when PATCH requested with already-archived file", async () => {
+  it("returns error 400 when PATCH requested with already non-archived file", async () => {
     const INPUT_DATA: FilesSetIsArchivedData = {
-      fileIds: [FILE_B_COMPONENT_ATLAS_WITH_ARCHIVED_LATEST.id],
+      fileIds: [SOURCE_DATASET_ATLAS_LINKED_A_FOO.file.id],
     };
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
           INPUT_DATA,
           USER_CONTENT_ADMIN,
           METHOD.PATCH,
@@ -251,7 +248,7 @@ describe(`${TEST_ROUTE}`, () => {
     };
     expect(
       (
-        await doArchiveRequest(
+        await doUnarchiveRequest(
           ATLAS_WITH_MISC_SOURCE_STUDIES.id,
           INPUT_DATA,
           USER_CONTENT_ADMIN,
@@ -262,45 +259,14 @@ describe(`${TEST_ROUTE}`, () => {
     ).toEqual(404);
   });
 
-  it("returns error 404 when PATCH requested with various files not on the atlas", async () => {
-    const WRONG_ATLAS_FILE_IDS = [
-      FILE_MANIFEST_FOO.id,
-      SOURCE_DATASET_FOO.file.id,
-      SOURCE_DATASET_BAR.file.id,
-      COMPONENT_ATLAS_DRAFT_FOO.file.id,
-    ];
-    const RIGHT_ATLAS_FILE_IDS = [
-      SOURCE_DATASET_ATLAS_LINKED_B_BAR.file.id,
-      COMPONENT_ATLAS_MISC_BAR.file.id,
-    ];
-    const INPUT_DATA: FilesSetIsArchivedData = {
-      fileIds: [...WRONG_ATLAS_FILE_IDS, ...RIGHT_ATLAS_FILE_IDS],
-    };
-    const res = await doArchiveRequest(
-      ATLAS_WITH_MISC_SOURCE_STUDIES.id,
-      INPUT_DATA,
-      USER_CONTENT_ADMIN,
-      METHOD.PATCH,
-      true
-    );
-    expect(res._getStatusCode()).toEqual(404);
-    const { message } = res._getJSONData();
-    for (const id of WRONG_ATLAS_FILE_IDS) {
-      expect(message).toEqual(expect.stringContaining(id));
-    }
-    for (const id of RIGHT_ATLAS_FILE_IDS) {
-      expect(message).not.toEqual(expect.stringContaining(id));
-    }
-  });
-
   it("returns error 400 when PATCH requested with empty file list", async () => {
     const INPUT_DATA: FilesSetIsArchivedData = {
       fileIds: [],
     };
     expect(
       (
-        await doArchiveRequest(
-          ATLAS_WITH_MISC_SOURCE_STUDIES.id,
+        await doUnarchiveRequest(
+          ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
           INPUT_DATA,
           USER_CONTENT_ADMIN,
           METHOD.PATCH,
@@ -311,84 +277,84 @@ describe(`${TEST_ROUTE}`, () => {
   });
 
   it("archives source dataset file when PATCH requested by user with INTEGRATION_LEAD role for the atlas", async () => {
-    const INPUT_DATA = ARCHIVE_DATA_SOURCE_DATASET_A_BAR;
-    const res = await doArchiveRequest(
-      ATLAS_WITH_MISC_SOURCE_STUDIES.id,
+    const INPUT_DATA = UNARCHIVE_DATA_SOURCE_DATASET_ARCHIVED_FOO;
+    const res = await doUnarchiveRequest(
+      ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
       INPUT_DATA,
-      USER_INTEGRATION_LEAD_WITH_MISC_SOURCE_STUDIES,
+      USER_INTEGRATION_LEAD_WITH_MISC_SOURCE_STUDIES_B,
       METHOD.PATCH,
       false
     );
     expect(res._getStatusCode()).toEqual(200);
-    await expectFilesToHaveArchiveStatus(INPUT_DATA.fileIds, true);
+    await expectFilesToHaveArchiveStatus(INPUT_DATA.fileIds, false);
     await expectFilesToHaveArchiveStatus(
-      [SOURCE_DATASET_ATLAS_LINKED_A_FOO.file.id],
-      false
+      [SOURCE_DATASET_ARCHIVED_FOOFOO.file.id],
+      true
     );
   });
 
   it("archives source dataset file when PATCH requested by user with CONTENT_ADMIN role", async () => {
     const INPUT_DATA: FilesSetIsArchivedData = {
-      fileIds: [SOURCE_DATASET_ATLAS_LINKED_B_FOO.file.id],
+      fileIds: [SOURCE_DATASET_ARCHIVED_FOOBAR.file.id],
     };
-    const res = await doArchiveRequest(
-      ATLAS_WITH_MISC_SOURCE_STUDIES.id,
+    const res = await doUnarchiveRequest(
+      ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
       INPUT_DATA,
       USER_CONTENT_ADMIN,
       METHOD.PATCH,
       false
     );
     expect(res._getStatusCode()).toEqual(200);
-    await expectFilesToHaveArchiveStatus(INPUT_DATA.fileIds, true);
+    await expectFilesToHaveArchiveStatus(INPUT_DATA.fileIds, false);
     await expectFilesToHaveArchiveStatus(
-      [SOURCE_DATASET_ATLAS_LINKED_A_FOO.file.id],
-      false
+      [SOURCE_DATASET_ARCHIVED_FOOFOO.file.id],
+      true
     );
   });
 
   it("archives component atlas file when PATCH requested by user with CONTENT_ADMIN role", async () => {
-    const INPUT_DATA = ARCHIVE_DATA_COMPONENT_ATLAS_MISC_FOO;
-    const res = await doArchiveRequest(
-      ATLAS_WITH_MISC_SOURCE_STUDIES.id,
+    const INPUT_DATA = UNARCHIVE_DATA_COMPONENT_ATLAS_ARCHIVED_FOO;
+    const res = await doUnarchiveRequest(
+      ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
       INPUT_DATA,
       USER_CONTENT_ADMIN,
       METHOD.PATCH,
       false
     );
     expect(res._getStatusCode()).toEqual(200);
-    await expectFilesToHaveArchiveStatus(INPUT_DATA.fileIds, true);
+    await expectFilesToHaveArchiveStatus(INPUT_DATA.fileIds, false);
     await expectFilesToHaveArchiveStatus(
-      [COMPONENT_ATLAS_WITH_CELLXGENE_DATASETS.file.id],
-      false
+      [COMPONENT_ATLAS_ARCHIVED_FOOFOO.file.id],
+      true
     );
   });
 
   it("archives multiple source dataset and component atlas files when PATCH requested by user with CONTENT_ADMIN role", async () => {
     const INPUT_DATA: FilesSetIsArchivedData = {
       fileIds: [
-        SOURCE_DATASET_ATLAS_LINKED_B_BAR.file.id,
-        COMPONENT_ATLAS_MISC_BAR.file.id,
-        COMPONENT_ATLAS_MISC_BAZ.file.id,
-        SOURCE_DATASET_PUBLISHED_WITHOUT_CELLXGENE_ID_FOO.file.id,
+        SOURCE_DATASET_ARCHIVED_BAR.file.id,
+        COMPONENT_ATLAS_ARCHIVED_BAR.file.id,
+        COMPONENT_ATLAS_ARCHIVED_BAZ.file.id,
+        SOURCE_DATASET_ARCHIVED_BAZ.file.id,
       ],
     };
-    const res = await doArchiveRequest(
-      ATLAS_WITH_MISC_SOURCE_STUDIES.id,
+    const res = await doUnarchiveRequest(
+      ATLAS_WITH_MISC_SOURCE_STUDIES_B.id,
       INPUT_DATA,
       USER_CONTENT_ADMIN,
       METHOD.PATCH,
       false
     );
     expect(res._getStatusCode()).toEqual(200);
-    await expectFilesToHaveArchiveStatus(INPUT_DATA.fileIds, true);
+    await expectFilesToHaveArchiveStatus(INPUT_DATA.fileIds, false);
     await expectFilesToHaveArchiveStatus(
-      [SOURCE_DATASET_ATLAS_LINKED_A_FOO.file.id],
-      false
+      [SOURCE_DATASET_ARCHIVED_FOOFOO.file.id],
+      true
     );
   });
 });
 
-async function doArchiveRequest(
+async function doUnarchiveRequest(
   atlasId: string,
   body: Record<string, unknown>,
   user?: TestUser,
@@ -402,7 +368,7 @@ async function doArchiveRequest(
     query: getQueryValues(atlasId),
   });
   await withConsoleErrorHiding(
-    () => archiveHandler(req, res),
+    () => unarchiveHandler(req, res),
     hideConsoleError
   );
   return res;
