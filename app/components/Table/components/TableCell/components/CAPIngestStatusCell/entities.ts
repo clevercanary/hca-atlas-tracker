6c8907cf9b7e8e3a8662b046195c93f24ac05826
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-15 15:14:32 +1000

    fix: cap ingest status (#909)

--- app/components/Table/components/TableCell/components/CAPIngestStatusCell/entities.ts
+++ app/components/Table/components/TableCell/components/CAPIngestStatusCell/entities.ts
@@ -1,12 +1,14 @@
 import { CellContext } from "@tanstack/react-table";
-import {
-  FILE_VALIDATION_STATUS,
-  HCAAtlasTrackerComponentAtlas,
-} from "../../../../../../apis/catalog/hca-atlas-tracker/common/entities";
+import { HCAAtlasTrackerComponentAtlas } from "../../../../../../apis/catalog/hca-atlas-tracker/common/entities";
 import { AtlasSourceDataset } from "../../../../../../views/AtlasSourceDatasetsView/entities";
+
+export enum CAP_INGEST_STATUS {
+  CAP_READY = "CAP_READY",
+  NEEDS_VALIDATION = "NEEDS_VALIDATION",
+  NOT_REQUIRED = "NOT_REQUIRED",
+  UPDATES_REQUIRED = "UPDATES_REQUIRED",
+}
 
 export type Props =
   | CellContext<HCAAtlasTrackerComponentAtlas, unknown>
   | CellContext<AtlasSourceDataset, unknown>;
-
-export type CAP_INGEST_STATUS = "NOT_REQUIRED" | FILE_VALIDATION_STATUS;
--- app/components/Table/components/TableCell/components/CAPIngestStatusCell/utils.ts
+++ app/components/Table/components/TableCell/components/CAPIngestStatusCell/utils.ts
@@ -3,7 +3,10 @@ import { CHIP_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/chi
 import { ChipProps } from "@mui/material";
 import { Getter } from "@tanstack/react-table";
 import { ComponentProps } from "react";
-import { REPROCESSED_STATUS } from "../../../../../../apis/catalog/hca-atlas-tracker/common/entities";
+import {
+  FILE_VALIDATION_STATUS,
+  REPROCESSED_STATUS,
+} from "../../../../../../apis/catalog/hca-atlas-tracker/common/entities";
 import { CAP_INGEST_STATUS_COLOR, CAP_INGEST_STATUS_LABEL } from "./constants";
 import { CAP_INGEST_STATUS, Props } from "./entities";
 
@@ -16,19 +19,8 @@ import { CAP_INGEST_STATUS, Props } from "./entities";
 export function buildCAPIngestStatus({
   row,
 }: Pick<Props, "row">): ComponentProps<typeof ChipCell> {
-  const { original } = row;
-  const { validationStatus } = original;
-
   // Determine CAP ingest status.
-  let status: CAP_INGEST_STATUS = validationStatus;
-  // Status is "NOT_REQUIRED" for reprocessed source datasets.
-  if (
-    "reprocessedStatus" in original &&
-    original.reprocessedStatus === REPROCESSED_STATUS.REPROCESSED
-  ) {
-    status = "NOT_REQUIRED";
-  }
-
+  const status = getCapIngestStatus({ row });
   return {
     getValue: (() => {
       return {
@@ -38,4 +30,43 @@ export function buildCAPIngestStatus({
       };
     }) as Getter<ChipProps>,
   } as ComponentProps<typeof ChipCell>;
+}
+
+/**
+ * Determine CAP ingest status.
+ * @param cellContext - CellContext.
+ * @param cellContext.row - Row.
+ * @returns CAP ingest status.
+ */
+function getCapIngestStatus({ row }: Pick<Props, "row">): CAP_INGEST_STATUS {
+  const { original } = row;
+  const { validationStatus, validationSummary } = original;
+
+  // Determine CAP ingest status for source datasets with reprocessed status of "REPROCESSED" or "UNSPECIFIED".
+  if ("reprocessedStatus" in original) {
+    if (original.reprocessedStatus === REPROCESSED_STATUS.REPROCESSED) {
+      // Status is "NOT_REQUIRED" for reprocessed source datasets.
+      return CAP_INGEST_STATUS.NOT_REQUIRED;
+    }
+    if (original.reprocessedStatus === REPROCESSED_STATUS.UNSPECIFIED) {
+      // Status is "UPDATES_REQUIRED" for unspecified source datasets.
+      return CAP_INGEST_STATUS.UPDATES_REQUIRED;
+    }
+  }
+
+  // Determine CAP ingest status with validation status of "COMPLETED".
+  if (validationStatus === FILE_VALIDATION_STATUS.COMPLETED) {
+    // No validation summary available.
+    if (!validationSummary) {
+      return CAP_INGEST_STATUS.NEEDS_VALIDATION;
+    }
+    // Status is "CAP_READY" with completed, successful validation.
+    if (validationSummary.validators.cap) {
+      return CAP_INGEST_STATUS.CAP_READY;
+    }
+    // Status is "UPDATES_REQUIRED" with completed validation with errors.
+    return CAP_INGEST_STATUS.UPDATES_REQUIRED;
+  }
+
+  return CAP_INGEST_STATUS.NEEDS_VALIDATION;
 }
