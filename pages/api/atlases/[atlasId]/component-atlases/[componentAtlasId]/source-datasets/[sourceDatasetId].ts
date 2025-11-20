5181b325fe0128e4f096a8cdf22c4c1b4bbb7010
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-11-20 15:55:55 -0800

    feat: remove component atlas individual source dataset post and delete apis (#961)

--- pages/api/atlases/[atlasId]/component-atlases/[componentAtlasId]/source-datasets/[sourceDatasetId].ts
+++ pages/api/atlases/[atlasId]/component-atlases/[componentAtlasId]/source-datasets/[sourceDatasetId].ts
@@ -1,69 +1,33 @@
 import { dbSourceDatasetToApiSourceDataset } from "app/apis/catalog/hca-atlas-tracker/common/backend-utils";
 import { ROLE_GROUP } from "../../../../../../../app/apis/catalog/hca-atlas-tracker/common/constants";
-import { ROLE } from "../../../../../../../app/apis/catalog/hca-atlas-tracker/common/entities";
 import { METHOD } from "../../../../../../../app/common/entities";
-import {
-  addSourceDatasetsToComponentAtlas,
-  deleteSourceDatasetsFromComponentAtlas,
-} from "../../../../../../../app/services/component-atlases";
 import { getComponentAtlasSourceDataset } from "../../../../../../../app/services/source-datasets";
 import {
-  handleByMethod,
   handler,
-  integrationLeadAssociatedAtlasOnly,
+  method,
   role,
 } from "../../../../../../../app/utils/api-handler";
 
-const getHandler = handler(role(ROLE_GROUP.READ), async (req, res) => {
-  const atlasId = req.query.atlasId as string;
-  const componentAtlasId = req.query.componentAtlasId as string;
-  const sourceDatasetId = req.query.sourceDatasetId as string;
-  res
-    .status(200)
-    .json(
-      dbSourceDatasetToApiSourceDataset(
-        await getComponentAtlasSourceDataset(
-          atlasId,
-          componentAtlasId,
-          sourceDatasetId
-        )
-      )
-    );
-});
-
-const postHandler = handler(
-  role([ROLE.CONTENT_ADMIN, ROLE.INTEGRATION_LEAD]),
-  integrationLeadAssociatedAtlasOnly,
-  async (req, res) => {
-    const atlasId = req.query.atlasId as string;
-    const componentAtlasId = req.query.componentAtlasId as string;
-    const sourceDatasetId = req.query.sourceDatasetId as string;
-    await addSourceDatasetsToComponentAtlas(atlasId, componentAtlasId, [
-      sourceDatasetId,
-    ]);
-    res.status(201).end();
-  }
-);
-
-const deleteHandler = handler(
-  role([ROLE.CONTENT_ADMIN, ROLE.INTEGRATION_LEAD]),
-  integrationLeadAssociatedAtlasOnly,
-  async (req, res) => {
-    const atlasId = req.query.atlasId as string;
-    const componentAtlasId = req.query.componentAtlasId as string;
-    const sourceDatasetId = req.query.sourceDatasetId as string;
-    await deleteSourceDatasetsFromComponentAtlas(atlasId, componentAtlasId, [
-      sourceDatasetId,
-    ]);
-    res.status(200).end();
-  }
-);
-
 /**
- * API route for adding or deleting a source dataset on a component atlas.
+ * API route for getting a source dataset of a component atlas.
  */
-export default handleByMethod({
-  [METHOD.GET]: getHandler,
-  [METHOD.POST]: postHandler,
-  [METHOD.DELETE]: deleteHandler,
-});
+export default handler(
+  method(METHOD.GET),
+  role(ROLE_GROUP.READ),
+  async (req, res) => {
+    const atlasId = req.query.atlasId as string;
+    const componentAtlasId = req.query.componentAtlasId as string;
+    const sourceDatasetId = req.query.sourceDatasetId as string;
+    res
+      .status(200)
+      .json(
+        dbSourceDatasetToApiSourceDataset(
+          await getComponentAtlasSourceDataset(
+            atlasId,
+            componentAtlasId,
+            sourceDatasetId
+          )
+        )
+      );
+  }
+);
