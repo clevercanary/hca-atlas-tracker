771b0093f2a06692beb167d961af23d827fbd6e5
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-11-25 13:59:11 +1000

    feat: make a generic form component for viewcomponentatlas, atlasform and others (#999)

---
+++ app/components/Entity/components/EntityForm/entityForm.tsx
@@ -0,0 +1,48 @@
+import { ElementType, Fragment, ReactNode } from "react";
+import { FieldValues } from "react-hook-form";
+import { FormMethod } from "../../../../hooks/useForm/common/entities";
+import { FormManager as FormManagerProps } from "../../../../hooks/useFormManager/common/entities";
+import { Divider } from "../../../Detail/components/TrackerForm/components/Divider/divider.styles";
+import { TrackerFormSection as Section } from "../../../Detail/components/TrackerForm/components/Section/components/TrackerFormSection/trackerFormSection";
+import { TrackerForm } from "../../../Detail/components/TrackerForm/trackerForm";
+import { SectionConfig } from "../../../Forms/common/entities";
+import { FormManager } from "../../../common/Form/components/FormManager/formManager";
+
+interface EntityFormProps<
+  T extends FieldValues,
+  R = undefined,
+  C extends ElementType = "input"
+> {
+  accessFallback: ReactNode;
+  formManager: FormManagerProps;
+  formMethod: FormMethod<T, R>;
+  sectionConfigs: SectionConfig<T, R, C>[];
+}
+
+export const EntityForm = <
+  T extends FieldValues,
+  R = undefined,
+  C extends ElementType = "input"
+>({
+  accessFallback,
+  formManager,
+  formMethod,
+  sectionConfigs,
+}: EntityFormProps<T, R, C>): JSX.Element => {
+  if (accessFallback) return <Fragment>{accessFallback}</Fragment>;
+  return (
+    <TrackerForm>
+      <FormManager {...formManager} />
+      {sectionConfigs.map(({ showDivider, ...sectionConfig }, i) => (
+        <Fragment key={i}>
+          {(i !== 0 || showDivider) && <Divider />}
+          <Section<T, R, C>
+            formManager={formManager}
+            formMethod={formMethod}
+            {...sectionConfig}
+          />
+        </Fragment>
+      ))}
+    </TrackerForm>
+  );
+};
--- app/views/ComponentAtlasView/componentAtlasView.tsx
+++ app/views/ComponentAtlasView/componentAtlasView.tsx
@@ -2,14 +2,16 @@ import { ConditionalComponent } from "@databiosphere/findable-ui/lib/components/
 import { Fragment } from "react";
 import { HCAAtlasTrackerComponentAtlas } from "../../apis/catalog/hca-atlas-tracker/common/entities";
 import { PathParameter } from "../../common/entities";
+import { AccessPrompt } from "../../components/common/Form/components/FormManager/components/AccessPrompt/accessPrompt";
 import { shouldRenderView } from "../../components/Detail/common/utils";
 import { Breadcrumbs } from "../../components/Detail/components/TrackerForm/components/Breadcrumbs/breadcrumbs";
-import { ViewComponentAtlas } from "../../components/Detail/components/ViewComponentAtlas/viewComponentAtlas";
 import { Tabs } from "../../components/Entity/components/common/Tabs/tabs";
+import { EntityForm } from "../../components/Entity/components/EntityForm/entityForm";
 import { DetailView } from "../../components/Layout/components/Detail/detailView";
 import { Payload } from "../../hooks/UseEditFileArchived/entities";
 import { useFetchAtlas } from "../../hooks/useFetchAtlas";
 import { useFetchDataState } from "../../hooks/useFetchDataState";
+import { FormManager } from "../../hooks/useFormManager/common/entities";
 import { EntityProvider } from "../../providers/entity/provider";
 import { fetchData } from "../../providers/fetchDataState/actions/fetchData/dispatch";
 import { VIEW_INTEGRATED_OBJECT_SECTION_CONFIGS } from "./common/sections";
@@ -70,7 +72,8 @@ export const ComponentAtlasView = ({
             />
           }
           mainColumn={
-            <ViewComponentAtlas
+            <EntityForm
+              accessFallback={renderAccessFallback(formManager)}
               formManager={formManager}
               formMethod={formMethod}
               sectionConfigs={VIEW_INTEGRATED_OBJECT_SECTION_CONFIGS}
@@ -97,4 +100,17 @@ export const ComponentAtlasView = ({
  */
 function mapPayload(integratedObject: HCAAtlasTrackerComponentAtlas): Payload {
   return { fileIds: [integratedObject.fileId] };
+}
+
+/**
+ * Returns the access fallback component from the form manager access state.
+ * @param formManager - Form manager.
+ * @returns access fallback component.
+ */
+function renderAccessFallback(formManager: FormManager): JSX.Element | null {
+  const {
+    access: { canView },
+  } = formManager;
+  if (!canView) return <AccessPrompt text="to view the integrated object" />;
+  return null;
 }
