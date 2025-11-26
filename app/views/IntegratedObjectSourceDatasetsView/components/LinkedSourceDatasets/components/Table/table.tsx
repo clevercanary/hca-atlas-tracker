a4a0c1b34486340d9a62f4b856a5a653157b8eb7
Fran McDade <frano-m@users.noreply.github.com>
2025-11-26 16:45:45 +1000

    refactor: move linkedsourcedatasets to integrated object source datasets (#1002) (#1031)
    Co-authored-by: Fran McDade <18710366+frano-m@users.noreply.github.com>
    Co-authored-by: Hunter Craft <118154470+hunterckx@users.noreply.github.com>

--- app/views/IntegratedObjectSourceDatasetsView/components/LinkedSourceDatasets/components/Table/table.tsx
+++ app/views/IntegratedObjectSourceDatasetsView/components/LinkedSourceDatasets/components/Table/table.tsx
@@ -1,8 +1,8 @@
-import { HCAAtlasTrackerSourceDataset } from "../../../../../../../../../../../../apis/catalog/hca-atlas-tracker/common/entities";
-import { PathParameter } from "../../../../../../../../../../../../common/entities";
-import { getAtlasComponentSourceDatasetsTableColumns } from "../../../../../../../../../../../../viewModelBuilders/catalog/hca-atlas-tracker/common/viewModelBuilders";
-import { useUnlinkComponentAtlasSourceDatasets } from "../../../../../../../../../../../../views/ComponentAtlasView/hooks/useUnlinkComponentAtlasSourceDatasets";
-import { SectionTable } from "../../../../../../section.styles";
+import { HCAAtlasTrackerSourceDataset } from "../../../../../../apis/catalog/hca-atlas-tracker/common/entities";
+import { PathParameter } from "../../../../../../common/entities";
+import { SectionTable } from "../../../../../../components/Detail/components/TrackerForm/components/Section/section.styles";
+import { getAtlasComponentSourceDatasetsTableColumns } from "../../../../../../viewModelBuilders/catalog/hca-atlas-tracker/common/viewModelBuilders";
+import { useUnlinkComponentAtlasSourceDatasets } from "../../../../../ComponentAtlasView/hooks/useUnlinkComponentAtlasSourceDatasets";
 import { TABLE_OPTIONS } from "./common/constants";
 import { getGridTemplateColumns } from "./common/utils";
 
