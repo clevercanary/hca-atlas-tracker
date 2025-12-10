a5cc1207017b5dae870de227ffc81ea860379178
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-12-09 18:11:11 -0800

    feat: add migration to move component atlas links to atlases table (#1046)

---
+++ migrations/1765326730188_component-atlases-array.ts
@@ -0,0 +1,64 @@
+import { MigrationBuilder } from "node-pg-migrate";
+
+export function up(pgm: MigrationBuilder): void {
+  pgm.addColumns(
+    { name: "atlases", schema: "hat" },
+    {
+      component_atlases: {
+        default: "{}",
+        notNull: true,
+        type: "uuid[]",
+      },
+    }
+  );
+
+  pgm.sql(
+    `
+      UPDATE hat.atlases a
+      SET component_atlases = (
+        SELECT COALESCE(array_agg(c.id), '{}') FROM hat.component_atlases c WHERE c.atlas_id=a.id
+      )
+    `
+  );
+
+  pgm.dropConstraint(
+    { name: "component_atlases", schema: "hat" },
+    "pk_component_atlases_atlas_id"
+  );
+
+  pgm.dropColumn({ name: "component_atlases", schema: "hat" }, "atlas_id");
+}
+
+export function down(pgm: MigrationBuilder): void {
+  pgm.addColumns(
+    { name: "component_atlases", schema: "hat" },
+    {
+      atlas_id: {
+        notNull: false, // Different from the original column, in order to support component atlas rows that may not be referenced by any atlas
+        type: "uuid",
+      },
+    }
+  );
+
+  pgm.addConstraint(
+    { name: "component_atlases", schema: "hat" },
+    "pk_component_atlases_atlas_id",
+    {
+      foreignKeys: {
+        columns: "atlas_id",
+        references: { name: "atlases", schema: "hat" },
+      },
+    }
+  );
+
+  pgm.sql(
+    `
+      UPDATE hat.component_atlases c
+      SET atlas_id = (
+        SELECT a.id FROM hat.atlases a WHERE c.id = ANY(a.component_atlases) LIMIT 1
+      )
+    `
+  );
+
+  pgm.dropColumn({ name: "atlases", schema: "hat" }, "component_atlases");
+}
