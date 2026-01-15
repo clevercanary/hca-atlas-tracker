6f214aed50cbb486beb897d9d9bbcee8a4ac18db
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-01-14 16:52:49 -0800

    docs: add doc comment for `updateComponentAtlasVersionInAtlases` (#1043)

--- app/data/atlases.ts
+++ app/data/atlases.ts
@@ -1,5 +1,11 @@
 import pg from "pg";
 
+/**
+ * Update any atlases that contain a given existing component atlas version to instead contain a different given version.
+ * @param existingVersionId - Existing component atlas version ID to replace.
+ * @param newVersionId - New component atlas version ID to insert.
+ * @param client - Postgres client to use.
+ */
 export async function updateComponentAtlasVersionInAtlases(
   existingVersionId: string,
   newVersionId: string,
