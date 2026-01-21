2208544f0ca4ee7ac693a276d9a6acd926a503dd
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-01-21 14:25:34 -0800

    revert: "test: use version ids for source dataset references in test entities (#1069)"
    This reverts commit 601cce1eb7e83cf6e88731f3111d878e517cf6e4.

--- testing/db-utils.ts
+++ testing/db-utils.ts
@@ -185,7 +185,7 @@ async function initComponentAtlases(client: pg.PoolClient): Promise<void> {
         info,
         componentAtlas.id,
         componentAtlas.versionId,
-        componentAtlas.sourceDatasets?.map((d) => d.versionId) ?? [],
+        componentAtlas.sourceDatasets?.map((d) => d.id) ?? [],
         componentAtlas.file.id,
         componentAtlas.wipNumber ?? 1,
         componentAtlas.isLatest ?? true,
