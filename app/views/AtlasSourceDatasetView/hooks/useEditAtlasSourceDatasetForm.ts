bd2da87d081d86993b05b5218c030be721db2ea6
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-02-18 21:26:32 -0800

    feat: add version to source dataset detail (#1108)

--- app/views/AtlasSourceDatasetView/hooks/useEditAtlasSourceDatasetForm.ts
+++ app/views/AtlasSourceDatasetView/hooks/useEditAtlasSourceDatasetForm.ts
@@ -8,6 +8,7 @@ import { FIELD_NAME } from "../common/constants";
 import { ViewAtlasSourceDatasetData } from "../common/entities";
 import { viewAtlasSourceDatasetSchema } from "../common/schema";
 import { useFetchAtlasSourceDataset } from "./useFetchAtlasSourceDataset";
+import { getApiEntityFileVersion } from "app/apis/catalog/hca-atlas-tracker/common/utils";
 
 const SCHEMA = viewAtlasSourceDatasetSchema;
 
@@ -42,5 +43,6 @@ function mapSchemaValues(
     [FIELD_NAME.SIZE_BYTES]: formatFileSize(sourceDataset.sizeBytes),
     [FIELD_NAME.TITLE]: sourceDataset.title,
     [FIELD_NAME.VALIDATION_STATUS]: sourceDataset.validationStatus,
+    [FIELD_NAME.VERSION]: getApiEntityFileVersion(sourceDataset),
   };
 }
