30651af09c8562ee29783316e0fcfab549eeccdd
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-02-24 17:44:15 -0800

    feat: add api to publish an atlas (#1116)

---
+++ pages/api/atlases/[atlasId]/publish.ts
@@ -0,0 +1,19 @@
+import { ROLE } from "../../../../app/apis/catalog/hca-atlas-tracker/common/entities";
+import { METHOD } from "../../../../app/common/entities";
+import { publishAtlas } from "../../../../app/services/atlases";
+import { handler, role } from "../../../../app/utils/api-handler";
+import { method } from "../../../../app/utils/api-handler";
+
+/**
+ * API route to publish an atlas.
+ */
+
+export default handler(
+  method(METHOD.POST),
+  role(ROLE.CONTENT_ADMIN),
+  async (req, res) => {
+    const id = req.query.atlasId as string;
+    await publishAtlas(id);
+    res.status(200).end();
+  },
+);
