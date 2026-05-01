24c7771fe6a51ca52ba20073bd2eab63c365f04f
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-05-01 13:02:03 -0700

    docs: add note regarding draft atlas requirement to test file generator (#1154)

--- db_scripts/generate-test-files.ts
+++ db_scripts/generate-test-files.ts
@@ -43,7 +43,7 @@ import { createSourceDataset } from "../app/services/source-datasets";
 /**
  * Usage: `npx esrun db_scripts/generate-test-files.ts`
  * Any number of existing entity IDs can be added as arguments to the command; if none are specified, new atlases will be generated.
- * Specified entity IDs may be for atlases, component atlases, or source datasets.
+ * Specified entity IDs may be for atlases (draft only), component atlases, or source datasets.
  * File entries will be randomly generated for the given or generated atlases.
  * New file versions will be generated for the given component atlases and source datasets.
  */
