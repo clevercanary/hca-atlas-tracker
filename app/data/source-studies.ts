63bca926409728a8bc1def8ffb5aa29fed08a758
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-05-24 21:33:12 -0700

    fix: properly join with both atlases and source datasets (#1272)

--- app/data/source-studies.ts
+++ app/data/source-studies.ts
@@ -27,30 +27,39 @@ export async function getInitialJoinSourceStudiesForGlobalAPI(
             PARTITION BY ar.overview->>'network', ar.overview->>'shortName', ar.generation
           ) AS max_revision
         FROM hat.atlases ar
-      )
-      SELECT
-        s.*,
-        COUNT(d.*)::int AS source_dataset_count,
-        ARRAY_AGG(
-          jsonb_build_object(
-            'generation', a.generation,
-            'id', a.id,
-            'isPrimary', FALSE,
-            'isLatest', a.revision = a.max_revision,
-            'network', a.overview->>'network',
-            'revision', a.revision,
-            'shortName', a.overview->>'shortName'
-          )
-        ) as atlases
-      FROM hat.source_studies s
-      LEFT JOIN (
-        SELECT fd.id, fd.source_study_id
+      ),
+      source_dataset_counts AS (
+        SELECT fd.source_study_id, COUNT(*)::int AS source_dataset_count
         FROM hat.source_datasets fd
         JOIN hat.files f ON f.id = fd.file_id
         WHERE NOT f.is_archived
-      ) as d ON d.source_study_id=s.id
-      JOIN atlases_with_revisions a ON a.source_studies ? s.id::text
-      GROUP BY s.id
+        GROUP BY fd.source_study_id
+      ),
+      study_atlases AS (
+        SELECT
+          s.id AS source_study_id,
+          ARRAY_AGG(
+            jsonb_build_object(
+              'generation', a.generation,
+              'id', a.id,
+              'isPrimary', FALSE,
+              'isLatest', a.revision = a.max_revision,
+              'network', a.overview->>'network',
+              'revision', a.revision,
+              'shortName', a.overview->>'shortName'
+            )
+          ) AS atlases
+        FROM hat.source_studies s
+        JOIN atlases_with_revisions a ON a.source_studies ? s.id::text
+        GROUP BY s.id
+      )
+      SELECT
+        s.*,
+        COALESCE(sdc.source_dataset_count, 0) AS source_dataset_count,
+        sa.atlases
+      FROM hat.source_studies s
+      JOIN study_atlases sa ON sa.source_study_id = s.id
+      LEFT JOIN source_dataset_counts sdc ON sdc.source_study_id = s.id
     `,
   );
   return queryResult.rows;
