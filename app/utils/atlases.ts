b2d6aa4661273554a888da142f9513bdd5165b0c
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-08-07 14:51:01 -0700

    refactor: resolve `sonarjs/concise-regex` linting error (#1382)

--- app/utils/atlases.ts
+++ app/utils/atlases.ts
@@ -55,8 +55,7 @@ export function parseS3AtlasVersion(s3Version: string): AtlasVersionNumbers {
     };
 
   // Major-minor, no leading zeros in either part except for minor version 0
-  // eslint-disable-next-line sonarjs/concise-regex -- track via #1382
-  const match = /^([1-9]\d*)-(0|[1-9][0-9]*)$/.exec(v);
+  const match = /^([1-9]\d*)-(0|[1-9]\d*)$/.exec(v);
   if (match)
     return {
       generation: Number(match[1]),
